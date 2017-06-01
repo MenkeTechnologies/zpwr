b2754dd1c66e0bd0a0af88f5e22f90036f47be80
MenkeTechnologies <linux.dev25@gmail.com>
2017-05-31 22:55:54 -0400

    ycm

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
-
 #!/usr/bin/env bash
+#Created by jacobmenke at Wed May 31 22:54:32 EDT 2017
 set -x
 
 os=`uname -s`
 
 printf "\e[1m"
+
+################################################################################
+## Dependencies
+################################################################################
+
 
 
 if [[ "$os" = "Darwin" ]]; then
@@ -49,6 +54,10 @@ if [[ "$os" = "Darwin" ]]; then
 		echo brew install cmatrix
 	fi
 
+	if [[ -z "$(which htop)" ]]; then
+		echo brew install htop
+	fi
+
 
 
 
@@ -67,8 +76,14 @@ else
 
 fi
 
+
+
 printf "Installing Powerline..."
 
+
+################################################################################
+## Zsh
+################################################################################
 echo pip install --user powerline-status
 
 printf "Installing oh-my-zsh..."
@@ -78,17 +93,40 @@ echo sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mast
 echo cp agnosterzak.zsh-theme $HOME/.oh-my-zsh/themes/
 echo cat zshrc >> $HOME/.zshrc
 
+#add aliases and functions
+echo cat ShellAliasesFunctions >> $HOME/.zshrc
+
+################################################################################
+## Vim
+################################################################################
 #install vundle
 if [[ ! -d $HOME/.vim/bundle ]]; then
 	echo mkdir -p $HOME/.vim/bundle
 fi
 echo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-	
+
+#youcompleteme, ultisnips, supertab
 echo cat vimrc >> $HOME/.vimrc
 echo vim +PluginInstall +qall
 
+#install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+#install nerdtree
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+#install ctrl p
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 
 
+
+
+
+################################################################################
+## Tmux
+################################################################################
+printf "Installing Tmux Config"
 
 
 tmuxPowerlineDir=$HOME/.config/powerline/themes/tmux
@@ -99,12 +137,16 @@ if [[ ! -d $tmuxPowerlineDir ]]; then
 fi
 
 
-#add aliases and functions
-echo cat ShellAliasesFunctions >> $HOME/.zshrc
+
+if [[ ! -d ~/.tmux/plugins/tpm  ]]; then
+	echo mkdir -p ~/.tmux/plugins/tpm
+	echo git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+echo "cat tmux/tmux.conf >> $HOME/.tmux.conf"
+echo "cat tmux/four-panes >> $HOME/.tmux/four-panes"
+echo "cat tmux/control-window >> $HOME/.tmux/control-window"
 
 
-printf "Installing Tmux Config"
-echo cat tmux.conf >> $HOME/.tmux.conf
 
 echo ch -s "$(which zsh)"
 
