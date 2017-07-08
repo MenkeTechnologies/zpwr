349eaa4055b493395582f12aa9f9f83dfd1e4dd5
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-08 00:37:58 -0400

    up

--- install.sh
+++ install.sh
@@ -99,7 +99,16 @@ fi
 printf "Installing Vim8\n"
 git clone https://github.com/vim/vim.git vim-master
 cd vim-master
-./configure
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp=yes \
+            --enable-pythoninterp=yes \
+            --with-python-config-dir=/usr/lib/python2.7/config \
+            --enable-python3interp=yes \
+            --with-python3-config-dir=/usr/lib/python3.5/config \
+            --enable-perlinterp=yes \
+            --enable-luainterp=yes \
+            --enable-gui=gtk2 --enable-cscope --prefix=/usr
 make -j 4
 sudo make install
 
