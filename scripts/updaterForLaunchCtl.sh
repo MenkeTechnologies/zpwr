32a886ce2cc29b97774b48af718cc1049dd0aaee
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-10 19:31:43 -0400

    update

--- scripts/updaterForLaunchCtl.sh
+++ scripts/updaterForLaunchCtl.sh
@@ -3,7 +3,7 @@
 #**************************************************************
 #####   Author: JACOBMENKE
 #####   Date: Mon Jul 10 19:30:53 EDT 2017
-#####   Purpose: bash script to 
+#####   Purpose: bash script to run updater scripts, output to logfile
 #####   Notes: 
 #}}}***********************************************************
 
--- scripts/uploadWebDS.sh
+++ scripts/uploadWebDS.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Mon Jul 10 19:31:35 EDT 2017
+#####   Purpose: bash script to u
+#####   Notes: 
+#}}}***********************************************************
 ADDRESS="jacobmenke@$IP:/var/services/web"
 
 echo -e "\033[44m\033[37mUploading $* to $ADDRESS"
 for i in "$@"; do
-	scp -r -P 7777 "$i" "$ADDRESS"
+    scp -r -P 7777 "$i" "$ADDRESS"
 done
 
 echo -e "Done\033[0m"
