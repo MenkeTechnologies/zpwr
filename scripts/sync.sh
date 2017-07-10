3a488dd019d176d6471b2e3abc9d9925482054cd
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-10 19:28:22 -0400

    update

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Mon Jul 10 19:28:12 EDT 2017
+#####   Purpose: bash script to  
+#####   Notes: 
+#}}}***********************************************************
 firstFile="$1"
 
 #exit is argument count less than 2
