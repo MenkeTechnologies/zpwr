01a8927230ddea5ad5fdc8aeb7780db4a2c436f1
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-10 12:07:06 -0400

    update

--- scripts/stresser.sh
+++ scripts/stresser.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-#very dangerous way to stress your system
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Mon Jul 10 12:06:21 EDT 2017
+#####   Purpose: bash script to stress system 
+#####   Notes: high CPU usage
+#}}}***********************************************************
 for (( i = 0; i < 10; i++ )); do
 	#launch yes in the background in subshell disowning it
 	#send all output to /dev/null
