8c423990bef04bc02ac3bacb1b657decca43f9ee
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-10 19:37:44 -0400

    update

--- scripts/watchtree2.sh
+++ scripts/watchtree2.sh
@@ -3,7 +3,7 @@
 #**************************************************************
 #####   Author: JACOBMENKE
 #####   Date: Mon Jul 10 19:36:58 EDT 2017
-#####   Purpose: bash script to 
+#####   Purpose: bash script to monitor tree of processes
 #####   Notes: 
 #}}}***********************************************************
 bash myWatchNoBlink.sh -b 'pstree -g 3 -u jacobmenke | sed s/jacobmenke// | sed s@/.*/@@ | tail -75' 1
