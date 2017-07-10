9619bffce24ba98a99fcd159afb44fa7bf7e6d34
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-10 12:21:34 -0400

    update

--- scripts/iostatShower.sh
+++ scripts/iostatShower.sh
@@ -3,7 +3,7 @@
 #**************************************************************
 #####   Author: JACOBMENKE
 #####   Date: Mon Jul 10 12:21:04 EDT 2017
-#####   Purpose: bash  script to  
+#####   Purpose: bash  script to show io stats
 #####   Notes: 
 #}}}***********************************************************
 
--- scripts/istatsShower.sh
+++ scripts/istatsShower.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Mon Jul 10 12:21:22 EDT 2017
+#####   Purpose: bash script to  
+#####   Notes: 
+#}}}***********************************************************
 
 bash "$SCRIPTS/mywatch.sh" istats
