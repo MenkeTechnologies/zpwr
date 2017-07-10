b9eb7411936a0967a0ad0a1e06becc34794a6d58
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-10 19:31:05 -0400

    update

--- scripts/updaterForLaunchCtl.sh
+++ scripts/updaterForLaunchCtl.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Mon Jul 10 19:30:53 EDT 2017
+#####   Purpose: bash script to 
+#####   Notes: 
+#}}}***********************************************************
 
 bash $HOME/Documents/shellScripts/updater.sh > $HOME/updaterlog.txt 2>&1
