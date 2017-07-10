0aaf1b28a459567018f3418f606ef9dfe5f9f0b1
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-10 12:13:14 -0400

    update

--- scripts/boldText.sh
+++ scripts/boldText.sh
@@ -3,8 +3,8 @@
 #**************************************************************
 #####   Author: JACOBMENKE
 #####   Date: Mon Jul 10 12:12:23 EDT 2017
-#####   Purpose: bash script to 
-#####   Notes: 
+#####   Purpose: bash script to bold text
+#####   Notes: usage | boldText.sh etc
 #}}}***********************************************************
 #escape sequences
 BLUE='\e[37;44m'
--- scripts/connectionShower.sh
+++ scripts/connectionShower.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Mon Jul 10 12:13:07 EDT 2017
+#####   Purpose: bash script to 
+#####   Notes: 
+#}}}***********************************************************
+
 bash myWatchNoBlink.sh "lsof -i | sed 's/jacobmenke//'" 1
