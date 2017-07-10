110398641089944d5658366ddb48296018b4074c
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-10 12:06:18 -0400

    update

--- scripts/tor.sh
+++ scripts/tor.sh
@@ -3,7 +3,7 @@
 #**************************************************************
 #####   Author: JACOBMENKE
 #####   Date: Mon Jul 10 12:05:46 EDT 2017
-#####   Purpose: bash script to  
+#####   Purpose: bash script to use tor network
 #####   Notes: 
 #}}}***********************************************************
 prettyPrint (){
