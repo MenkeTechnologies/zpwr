1277bf55db372cbfd95b8a021148dcb72893a7eb
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-10 12:20:45 -0400

    update

--- scripts/goForward.sh
+++ scripts/goForward.sh
@@ -3,8 +3,8 @@
 #**************************************************************
 #####   Author: JACOBMENKE
 #####   Date: Mon Jul 10 12:20:00 EDT 2017
-#####   Purpose: bash script to cd 
-#####   Notes: 
+#####   Purpose: bash script to cd to new directory and show contents 
+#####   Notes: must be sourced
 #}}}***********************************************************
 cd "$1"
 clear
--- scripts/info.sh
+++ scripts/info.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Mon Jul 10 12:20:35 EDT 2017
+#####   Purpose: bash script to 
+#####   Notes: 
+#}}}***********************************************************
 trap "tput cnorm ;exit" INT
 
 tput civis
