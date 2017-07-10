9bee4063aa483a4a7e44240c3216a8fe8577ff5e
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-10 12:13:39 -0400

    update

--- scripts/connectionShower.sh
+++ scripts/connectionShower.sh
@@ -3,7 +3,7 @@
 #**************************************************************
 #####   Author: JACOBMENKE
 #####   Date: Mon Jul 10 12:13:07 EDT 2017
-#####   Purpose: bash script to 
+#####   Purpose: bash script to continuously monitor open files
 #####   Notes: 
 #}}}***********************************************************
 
--- scripts/coolFormatter.sh
+++ scripts/coolFormatter.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Mon Jul 10 12:13:32 EDT 2017
+#####   Purpose: bash script to  
+#####   Notes: 
+#}}}***********************************************************
 #escape sequences
 BLUE='\e[37;44m'
 RESET='\e[0m'
