475eaad8c6f50a081f6ce5a676ccda8bca714ebc
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-11 00:19:14 -0400

    update

---
+++ scripts/upLoadPi2.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash 
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Mon Jul 10 19:30:30 EDT 2017
+#####   Purpose: bash  script to to upload to RPi
+#####   Notes: 
+#}}}***********************************************************
+echo -e "${BLUE}Uploading $*"
+for i in "$@"; do
+	scp -r -P 7780"$i" pi@$IP:~/Desktop
+
+done
+echo -e "Done${RESET}"
