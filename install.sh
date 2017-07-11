d4a94f0eae51c4bfdc4ffa3c23e3b75297b4e063
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-11 05:02:43 -0400

    update

--- install.sh
+++ install.sh
@@ -32,12 +32,12 @@ printf "\e[1m"
 # 16) powerline-mem-segment
 
 dependencies_ary=(vim tmux git wget lolcat cowsay cmatrix htop cmake glances bpython python-dev \
-    python3-dev colortail screenfetch \
-    libpcap-dev ncurses-dev iftop htop figlet silversearcher-ag zsh libevent-dev libncurses5-dev libgnome2-dev\
-    libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-    libcairo2-dev libx11-dev libxpm-dev libxt-dev \
-    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev rlwrap tor npm nginx nmap mtr tcpdump \
-    software-properties-common ctags speedtest-cli texinfo)
+	python3-dev colortail screenfetch \
+	libpcap-dev ncurses-dev iftop htop figlet silversearcher-ag zsh libevent-dev libncurses5-dev libgnome2-dev\
+	libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+	libcairo2-dev libx11-dev libxpm-dev libxt-dev \
+	python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev rlwrap tor npm nginx nmap mtr tcpdump \
+	software-properties-common ctags speedtest-cli texinfo)
 
 #}}}***********************************************************
 
@@ -46,55 +46,55 @@ dependencies_ary=(vim tmux git wget lolcat cowsay cmatrix htop cmake glances bpy
 
 update (){
 
-    if [[ -z "$(which $1)" ]]; then
-        if [[ $2 == mac ]]; then
-            brew install "$1"
-        else
-            sudo apt-get install -y "$1"
-        fi
-    else
-        printf "Already have $1\n"
-    fi
+	if [[ -z "$(which $1)" ]]; then
+		if [[ $2 == mac ]]; then
+			brew install "$1"
+		else
+			sudo apt-get install -y "$1"
+		fi
+	else
+		printf "Already have $1\n"
+	fi
 }
 
 #}}}***********************************************************
 
 if [[ "$OS_TYPE" == "Darwin" ]]; then
-    #{{{                    MARK:Mac
-    #**************************************************************
-    printf "Checking Dependencies for Mac...\n"
+	#{{{                    MARK:Mac
+	#**************************************************************
+	printf "Checking Dependencies for Mac...\n"
 
-    if [[ -z "$(which brew)" ]]; then
-        #install homebrew
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    fi
+	if [[ -z "$(which brew)" ]]; then
+		#install homebrew
+		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	fi
 
-    echo brew ls python > /dev/null 2>&1
-    if [[ $? == 1 ]]; then
-        brew install python
-        brew install pip
-    fi
+	echo brew ls python > /dev/null 2>&1
+	if [[ $? == 1 ]]; then
+		brew install python
+		brew install pip
+	fi
 
-    for prog in ${dependencies_ary[@]}; do
-        update $prog mac
-    done
+	for prog in ${dependencies_ary[@]}; do
+		update $prog mac
+	done
 
-    #}}}***********************************************************
+	#}}}***********************************************************
 
 else
 
-    #{{{                    MARK:Linux
-    #**************************************************************
+	#{{{                    MARK:Linux
+	#**************************************************************
 
-    printf "Installing Dependencies for Linux with APT...\n"
+	printf "Installing Dependencies for Linux with APT...\n"
 
-    sudo apt-get -y install build-essential
+	sudo apt-get -y install build-essential
 
-    for prog in ${dependencies_ary[@]}; do
-        update $prog linux
-    done
+	for prog in ${dependencies_ary[@]}; do
+		update $prog linux
+	done
 
-    #}}}***********************************************************
+	#}}}***********************************************************
 
 fi
 
@@ -102,21 +102,21 @@ fi
 #git clone https://github.com/vim/vim.git vim-master
 #cd vim-master
 #./configure --with-features=huge \
-    #--enable-multibyte \
-    #--enable-rubyinterp=yes \
-    #--enable-pythoninterp=yes \
-    #--with-python-config-dir=/usr/lib/python2.7/config \
-    #--enable-python3interp=yes \
-    #--with-python3-config-dir=/usr/lib/python3.5/config \
-    #--enable-perlinterp=yes \
-    #--enable-luainterp=yes \
-    #--enable-gui=gtk2 --enable-cscope --prefix=/usr
+	#--enable-multibyte \
+	#--enable-rubyinterp=yes \
+	#--enable-pythoninterp=yes \
+	#--with-python-config-dir=/usr/lib/python2.7/config \
+	#--enable-python3interp=yes \
+	#--with-python3-config-dir=/usr/lib/python3.5/config \
+	#--enable-perlinterp=yes \
+	#--enable-luainterp=yes \
+	#--enable-gui=gtk2 --enable-cscope --prefix=/usr
 #sudo make install
 
 printf "Installing Pathogen\n"
 #install pathogen
 mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle && \
-    curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+	curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 printf "Installing Vim Plugins\n"
 bash "./vim_plugins_install.sh"
@@ -190,8 +190,8 @@ bash "$INSTALLER_DIR/zsh_plugins_install.sh"
 
 printf "Installing Tmux Plugin Manager\n"
 if [[ ! -d ~/.tmux/plugins/tpm  ]]; then
-    echo mkdir -p ~/.tmux/plugins/tpm
-    echo git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+	echo mkdir -p ~/.tmux/plugins/tpm
+	echo git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 fi
 
 printf "Adding Powerline to .tmux.conf\n"
@@ -225,28 +225,32 @@ printf "Installing MyCLI\n"
 sudo pip install mycli
 
 if [[ ! -f "$HOME/.token.sh" ]]; then
-    touch "$HOME/.tokens.sh"
+	touch "$HOME/.tokens.sh"
 fi
 
 printf "HushLogin\n"
 if [[ ! -f "$HOME/.hushlogin" ]]; then
-    touch "$HOME/.hushlogin"
+	touch "$HOME/.hushlogin"
 fi
 
 if [[ ! -f "$HOME/.my.cnf" ]]; then
-    touch "$HOME/.my.cnf"
+	touch "$HOME/.my.cnf"
 fi
 
-printf "Changing pager to cat for MySQL"
+printf "Changing pager to cat for MySQL\n"
 echo "[client]" >> "$HOME/.my.cnf"
 echo "pager=cat" >> "$HOME/.my.cnf"
 
 echo "Copying all Shell Scripts..."
 cp $INSTALLER_DIR/*.sh $HOME/Documents/shellScripts
 
+printf "Installing ponysay from source\n"
+git clone https://github.com/erkin/ponysay.git
+cd ponysay && sudo ./setup.py --freedom=partial install
+
 type chsh >/dev/null 2>&1 && {
-    printf "Changing default shell to Zsh\n"
-    chsh -s "$(which zsh)"
+printf "Changing default shell to Zsh\n"
+chsh -s "$(which zsh)"
 }
 
 printf "Changing current shell to Zsh\n"
