cf42eed3df6c537daeaf186ab5431e4677040220
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-13 00:09:51 -0400

    update

--- scripts/upLoadPi.sh
+++ scripts/upLoadPi.sh
@@ -8,7 +8,7 @@
 #}}}***********************************************************
 echo -e "${BLUE}Uploading $*"
 for i in "$@"; do
-	scp -r -P 7778 "$i" pi@$IP:~/Desktop
+	scp -r -P 7778 "$i" pi@$IP:\$HOME/Desktop
 
 done
 echo -e "Done${RESET}"
