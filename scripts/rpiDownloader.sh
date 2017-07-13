b5f39b7ae23965faabbdc907bfb2b436e72c664e
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-13 00:10:20 -0400

    update

--- scripts/rpiDownloader.sh
+++ scripts/rpiDownloader.sh
@@ -9,7 +9,7 @@
 
 printf "${BLUE}Downloading $*\n"
 for i in "$@"; do
-	scp -P1234 -r "$i" jacobmenke@localhost\:$HOME/Desktop
+	scp -P1234 -r "$i" jacobmenke@localhost:\$HOME/Desktop
 	#scp -r "$i" jacobmenke@192.168.0.105:~/Desktop
 done
 printf "Done${RESET}\n"
