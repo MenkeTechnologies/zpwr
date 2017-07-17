fd31fc449bbd6f10689491fa9e27408439bc2c6f
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-17 10:37:26 -0400

    update

--- scripts/loggerPi.sh
+++ scripts/loggerPi.sh
@@ -3,7 +3,8 @@
 #**************************************************************
 #####   Author: JACOBMENKE
 #####   Date: Mon Jul 10 19:14:53 EDT 2017
-#####   Purpose: bash script to monitor all log files with color 
-#####   Notes: 
+#####   Purpose: bash script to monitor all log files with color
+#####   Notes: requires globstar option
 #}}}***********************************************************
-colortail -k /home/pi/.colortailconf -f /var/log/*.out /var/log/*.log /var/log/{dmesg,wtmp,debug,lastlog,messages} /var/log/*.err
+shopt -s globstar
+colortail -k /home/pi/.colortailconf -f /var/log/**/*.out /var/log/**/*.log /var/log/{dmesg,wtmp,debug,lastlog,messages} /var/log/**/*.err
--- scripts/macColorLoggers.sh
+++ scripts/macColorLoggers.sh
@@ -6,6 +6,7 @@
 #####   Purpose: bash script to monitor log files in color
 #####   Notes: 
 #}}}***********************************************************
+shopt -s globart
 
 colortail -k $HOME/.colortailconf -f /var/log/*.log /var/log/*.out 
 
