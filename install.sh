b5ee0e7d1d88fe713eb7054449b674219544d4e7
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-18 19:31:43 -0400

    up

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ printf "\e[1m"
 dependencies_ary=(vim tmux git wget lolcat cowsay cmatrix htop cmake glances bpython python-dev \
     python3-dev colortail screenfetch fortune postfix mailutils \
     libpcap-dev ncurses-dev iftop htop figlet silversearcher-ag zsh libevent-dev libncurses5-dev libgnome2-dev\
-    libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+    libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev netatalk \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev at dnsutils \
     python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev rlwrap tor npm nginx nmap mtr tcpdump \
     jnettop iotop atop software-properties-common ctags speedtest-cli texinfo lsof weechat)
