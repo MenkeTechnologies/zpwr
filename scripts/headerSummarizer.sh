dd746bef9709ceba7261c92b8912862651decf03
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-19 16:45:56 -0400

    fixed missing double quote in headerSummarizer

--- scripts/headerSummarizer.sh
+++ scripts/headerSummarizer.sh
@@ -8,7 +8,7 @@
 #}}}***********************************************************
 
 for file; do
-    purpose=$(sed -n 6p $file)"
+    purpose="$(sed -n 6p $file)"
     if [[ ! -z "$purpose" ]]; then
         echo "$(basename $file) $(sed -n 6p $file)"
     fi
