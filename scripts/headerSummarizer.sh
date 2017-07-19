363e6066a5e88bdb46b8b9624182593dd1327e30
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-19 16:36:58 -0400

    wrote headersummarizer script

---
+++ scripts/headerSummarizer.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Wed Jul 19 16:30:20 EDT 2017
+#####   Purpose: bash script to summarize shell scripts from headers
+#####   Notes: 
+#}}}***********************************************************
+
+for file; do
+    echo "$(basename $file) $(sed -n 6p $file)"
+    echo
+done
