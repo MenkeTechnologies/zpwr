06546a07a120fdceb2330b885c981d54a625cc56
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-20 16:08:21 -0400

    no longer monitoring /var/log/wtmp on rpi bc of some formatting errors

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 #created by JACOBMENKE at Wed Jun 28 21:45:18 EDT 2017 with /usr/local/bin/zsh
 
-say dogs
-say my pwd is `echo dogs`
 
-ls
 pwd
+
+
+echo 'the dogs are here'
+
+
+ls /
+
+
