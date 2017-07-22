49ea7c7ce455cf74b0a212830572a5ffe7a93018
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-21 22:07:51 -0400

    updated color logger script

--- scripts/colorLogger.sh
+++ scripts/colorLogger.sh
@@ -11,26 +11,37 @@ shopt -s globstar
 
 #tailVersion="colortail -k $HOME/.colortailconf"
 tailVersion=tail
+weHaveCCZE=no
 
 type ccze 1>/dev/null 2>&1 && {
-    pipeVersion=" | ccze"
+weHaveCCZE=yes
 }
 
 
-
 if [[ $(uname) == Darwin ]]; then
-  $tailVersion -f /var/log/**/*.log /var/log/**/*.out /var/log/DiagnosticMessages/*.asl \
-      /var/log/asl/*.asl /var/log/cups/* "$HOME"/Library/Logs/**/*.log "$HOME"/Library/Logs/**/*.out \
-        /Library/Logs/**/*.log $pipeVersion
+    if [[ $weHaveCCZE == yes ]]; then
+   $tailVersion -f /var/log/**/*.log /var/log/**/*.out \
+       /var/log/cups/* "$HOME"/Library/Logs/**/*.log "$HOME"/Library/Logs/**/*.out \
+        /Library/Logs/**/*.log | ccze
+   else
+   $tailVersion -f /var/log/**/*.log /var/log/**/*.out \
+       /var/log/cups/* "$HOME"/Library/Logs/**/*.log "$HOME"/Library/Logs/**/*.out \
+        /Library/Logs/**/*.log 
+    fi
 else
     #linux
     distroName=$(lsb_release -a | head -1 | awk '{print $3}')
 
     if [[ $distroName == Raspbian ]]; then
-        $tailVersion -f /var/log/**/*.log /var/log/{dmesg,debug,lastlog,messages} /var/log/**/*.err $pipeVersion
+
+        if [[ $weHaveCCZE == yes ]]; then
+            $tailVersion -f /var/log/**/*.log /var/log/{dmesg,debug,lastlog,messages} /var/log/**/*.err | ccze
+        else
+            $tailVersion -f /var/log/**/*.log /var/log/{dmesg,debug,lastlog,messages} /var/log/**/*.err
+        fi
     else 
         printf "Unsupported distro: $distroName...but trying anyways\n" >&2
-        $tailVersion -f /var/log/**/*.log /var/log/{dmesg,debug,lastlog,messages} /var/log/**/*.err $pipeVersion
+        $tailVersion -f /var/log/**/*.log /var/log/{dmesg,debug,lastlog,messages} /var/log/**/*.err
     fi
 
 fi
