4f6b291bd8b2eec6c3e364468210f6954363aed6
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-25 12:23:39 -0400

    edited shell aliases file to only source rpitokens when os is Raspbian

--- .shell_aliases_functions.sh
+++ .shell_aliases_functions.sh
@@ -16,27 +16,27 @@ export PATH="$PYEXECUTABLES:$SCRIPTS/save-run:$HOME/.local/bin:$HOME/perl5/bin:L
 export GITHUB_ACCOUNT='MenkeTechnologies'
 
 if [[ "$(uname)" == Darwin ]]; then
-	export WCC="/Volumes/JAKESD/wcc/cps"
-	export HOMEBREW_HOME_FORMULAE="/usr/local/Homebrew/Library/taps/homebrew/homebrew-core/formula"
-	export PATH="$SCRIPTS/macOnly:$HOME/.platformio/penv/bin:$PATH"
+    export WCC="/Volumes/JAKESD/wcc/cps"
+    export HOMEBREW_HOME_FORMULAE="/usr/local/Homebrew/Library/taps/homebrew/homebrew-core/formula"
+    export PATH="$SCRIPTS/macOnly:$HOME/.platformio/penv/bin:$PATH"
 else
-	export PATH="$PATH:/usr/games"
+    export PATH="$PATH:/usr/games"
 fi
 #}}}***********************************************************
 
 #{{{                           MARK:HOMES
 #**********************************************************************
 if [[ "$(uname)" == Darwin ]]; then
-	export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_141.jdk/Contents/Home"
-	export GROOVY_HOME="/usr/local/Cellar/groovy/2.4.11"
-	export SCALA_HOME="/usr/local/Cellar/scala/2.12.2"
-	export HOMEBREW_HOME='/usr/local/Cellar'
-	export PERL_HOME='$HOMEBREW_HOME/perl/5.24.1/'
-	export HOMEBREW_DBHOME='/usr/local/var'
-	export HOMEBREW_DB_CONF='/usr/local/etc'
-	eval `perl -I ~/perl5/lib/perl5 -Mlocal::lib`
-	export MANPATH=$HOME/perl5/man:$MANPATH
-	export TUTORIAL_FILES="$HOME/Documents/tutorialsRepo"
+    export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_141.jdk/Contents/Home"
+    export GROOVY_HOME="/usr/local/Cellar/groovy/2.4.11"
+    export SCALA_HOME="/usr/local/Cellar/scala/2.12.2"
+    export HOMEBREW_HOME='/usr/local/Cellar'
+    export PERL_HOME='$HOMEBREW_HOME/perl/5.24.1/'
+    export HOMEBREW_DBHOME='/usr/local/var'
+    export HOMEBREW_DB_CONF='/usr/local/etc'
+    eval `perl -I ~/perl5/lib/perl5 -Mlocal::lib`
+    export MANPATH=$HOME/perl5/man:$MANPATH
+    export TUTORIAL_FILES="$HOME/Documents/tutorialsRepo"
 fi
 export YARN_HOME="$HOME/.config/yarn"
 export NODE_PATH="/usr/local/lib/node_modules:$YARN_HOME/global/node_modules"
@@ -62,7 +62,7 @@ alias py="cd $PYSCRIPTS"
 alias p2="python"
 alias p3="python3"
 alias d="cd ~/Desktop"
-#{{{                    MARK:aliaes for editing config files
+#{{{                    MARK:ALIASES for editing config files
 #**************************************************************
 alias vrc="vim -S ~/.vim/sessions/vrc.vim ~/.vimrc"
 alias brc="vim -S ~/.vim/sessions/aliases.vim + ~/.shell_aliases_functions.sh; source ~/.shell_aliases_functions.sh; bash $SCRIPTS/backupBashConfig.sh 2> /dev/null"
@@ -79,24 +79,30 @@ alias tree='tree -afC'
 alias ta="tmux attach"
 #Darwin specific aliases
 if [[ "$(uname)" == "Darwin" ]]; then
-	#statements
-	alias spd="du -csh {.[^.]*,..?*} * 2> /dev/null | gsort -h"
-	alias cpu="top -o cpu"
-	alias mem="top -o mem"
-	alias tip="top -o +command"
-	alias nd="defaults write com.apple.dock autohide-delay -float 100 && defaults write com.apple.dock tilesize -int 1 && killall Dock"
-	alias back="nohup /System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background > /dev/null &"
-	alias n="open /Volumes/homes/JAKENAS/softwareTutorials; exit"
-	alias c="cd /Volumes/JAKESD/wcc/cps; clearList"
-	alias emu="open /Volumes/JAKESD/EMU"
-	alias pkill="pkill -iIl"
-	alias q="qlmanage -p &>/dev/null"
-	#keep remote tty sessions alive by stopping sleep
-	sudo pmset -c ttyskeepawake 1
+    #statements
+    alias spd="du -csh {.[^.]*,..?*} * 2> /dev/null | gsort -h"
+    alias cpu="top -o cpu"
+    alias mem="top -o mem"
+    alias tip="top -o +command"
+    alias nd="defaults write com.apple.dock autohide-delay -float 100 && defaults write com.apple.dock tilesize -int 1 && killall Dock"
+    alias back="nohup /System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background > /dev/null &"
+    alias n="open /Volumes/homes/JAKENAS/softwareTutorials; exit"
+    alias c="cd /Volumes/JAKESD/wcc/cps; clearList"
+    alias emu="open /Volumes/JAKESD/EMU"
+    alias pkill="pkill -iIl"
+    alias q="qlmanage -p &>/dev/null"
+    #keep remote tty sessions alive by stopping sleep
+    sudo pmset -c ttyskeepawake 1
 else
-	#Linux
-	alias apt="sudo apt-get install -y"
-	source "$HOME/.rpitokens.sh"
+    #Linux
+    alias apt="sudo apt-get install -y"
+    type lsb_release >/dev/null 2>&1 && {
+    distroName=$(lsb_release -a | head -1 | awk '{print $3}')
+    if [[ $distroName == Raspbian ]]; then
+        source "$HOME/.rpitokens.sh"
+    fi
+
+}
 fi
 alias cf2="sed 's/.*/_\U\l&_/' | boldText.sh | blue"
 alias tclsh="rlwrap tclsh"
@@ -170,213 +176,213 @@ export PATH="$HOME/.rvm/bin:$PATH" # Add RVM to PATH for scripting
 
 which yarn > /dev/null
 if [[ $? == 0 ]]; then
-	export PATH="$(yarn global bin):$PATH"
+    export PATH="$(yarn global bin):$PATH"
 fi
 
 #{{{                    MARK:Shell functions
 #**************************************************************
 scnew(){
-	if [[ -z "$1" ]];then
-		echo "no arg..."
-		return 1
-	fi
+    if [[ -z "$1" ]];then
+        echo "no arg..."
+        return 1
+    fi
 
-	bash '$HOME/Documents/shellScripts/createScriptButDontOpenSublime.sh' "$1"
+    bash '$HOME/Documents/shellScripts/createScriptButDontOpenSublime.sh' "$1"
 }
 nn(){
-	if [[ -z "$1" ]];then
-		echo "Title is \$1 and message is \$2..."
-		return 1
-	fi
+    if [[ -z "$1" ]];then
+        echo "Title is \$1 and message is \$2..."
+        return 1
+    fi
 
-	title="$1"
-	msg="$2"
+    title="$1"
+    msg="$2"
 
-	echo "display notification \"$msg\" with title \"$title\"" | osascript 
+    echo "display notification \"$msg\" with title \"$title\"" | osascript 
 
 }
 suc(){
-	subl $SCRIPTS
-	f $SCRIPTS
-	python3 $PYSCRIPTS/textEditorTwoColumns.py
+    subl $SCRIPTS
+    f $SCRIPTS
+    python3 $PYSCRIPTS/textEditorTwoColumns.py
 }
 db(){
-	#open -a Firefox $IP:8080/db
-	( python3 $PYSCRIPTS/logIntoMyDB.py & )
+    #open -a Firefox $IP:8080/db
+    ( python3 $PYSCRIPTS/logIntoMyDB.py & )
 }
 clearList () {
-	clear
-	if [[ "$(uname)" == "Darwin" ]]; then
-		ls -iFlhAO
-	else
-		ls -iFlhA
-	fi
+    clear
+    if [[ "$(uname)" == "Darwin" ]]; then
+        ls -iFlhAO
+    else
+        ls -iFlhA
+    fi
 }
 listNoClear () {
-	if [[ "$(uname)" == "Darwin" ]]; then
-		ls -iFlhAO
-	else
-		ls -iFlhA
-	fi
+    if [[ "$(uname)" == "Darwin" ]]; then
+        ls -iFlhAO
+    else
+        ls -iFlhA
+    fi
 }
 animate(){
-	bash $SCRIPTS/animation.sh
+    bash $SCRIPTS/animation.sh
 }
 blocksToSize(){
-	read input
-	local bytes=$(( input * 512 ))
-	echo $bytes | humanReadable 
+    read input
+    local bytes=$(( input * 512 ))
+    echo $bytes | humanReadable 
 }
 
 humanReadable(){
-	awk 'function human(x) {
-	s=" B   KiB MiB GiB TiB EiB PiB YiB ZiB"
-	while (x>=1024 && length(s)>1)
-		{x/=1024; s=substr(s,5)}
-		s=substr(s,1,4)
-		xf=(s==" B  ")?"%5d   ":"%8.2f"
-		return sprintf("Your size:"xf"%s", x, s)
-	}
-	{gsub(/^[0-9]+/, human($1));print}'
-	}
-	f(){
-		cd "$1"
-	}
-	execpy(){
-		python3 $PYSCRIPTS/"$1"
+    awk 'function human(x) {
+    s=" B   KiB MiB GiB TiB EiB PiB YiB ZiB"
+    while (x>=1024 && length(s)>1)
+        {x/=1024; s=substr(s,5)}
+        s=substr(s,1,4)
+        xf=(s==" B  ")?"%5d   ":"%8.2f"
+        return sprintf("Your size:"xf"%s", x, s)
+    }
+    {gsub(/^[0-9]+/, human($1));print}'
+    }
+    f(){
+        cd "$1"
+    }
+    execpy(){
+        python3 $PYSCRIPTS/"$1"
 
-	}
-	search(){
-		if [[ -z $2 ]];then
-			grep -iRnC 5 "$1" *
-		else
-			grep -iRnC 5 "$1" "$2"
-		fi
+    }
+    search(){
+        if [[ -z $2 ]];then
+            grep -iRnC 5 "$1" *
+        else
+            grep -iRnC 5 "$1" "$2"
+        fi
 
-	}
-	cd(){
-		#builtin is necessary here to distinguish bt function name and builtin cd command
-		#don't want to recursively call this function
-		builtin cd "$@";
-		clearList
-	}
-	gitCommitAndPush(){
-		printf "\e[1m"
-		/usr/local/bin/git add .
-		/usr/local/bin/git commit -m "$1"
-		/usr/local/bin/git push
-		printf "\e[0m"
-	}
-	replacer(){
-		orig="$1"
-		shift
-		replace="$1"
-		shift
-		sed -i'' "s/$orig/$replace/g" $@
+    }
+    cd(){
+        #builtin is necessary here to distinguish bt function name and builtin cd command
+        #don't want to recursively call this function
+        builtin cd "$@";
+        clearList
+    }
+    gitCommitAndPush(){
+        printf "\e[1m"
+        /usr/local/bin/git add .
+        /usr/local/bin/git commit -m "$1"
+        /usr/local/bin/git push
+        printf "\e[0m"
+    }
+    replacer(){
+        orig="$1"
+        shift
+        replace="$1"
+        shift
+        sed -i'' "s/$orig/$replace/g" $@
 
-	}
-	createGIF(){
-		outFile=out.gif
-		res=600x400
+    }
+    createGIF(){
+        outFile=out.gif
+        res=600x400
 
-		if [[ -z "$1" ]]; then
-			echo "One arg need..." >&2
-			return 1
-		fi
+        if [[ -z "$1" ]]; then
+            echo "One arg need..." >&2
+            return 1
+        fi
 
-		if [[ ! -z "$2" ]]; then
-			res="$2"
-		fi
+        if [[ ! -z "$2" ]]; then
+            res="$2"
+        fi
 
-		if [[ ! -z "$3" ]];then
-			outFile="$3"	
-		fi
+        if [[ ! -z "$3" ]];then
+            outFile="$3"	
+        fi
 
-		ffmpeg -i "$1" -s "$res" -pix_fmt rgb24 -r 10 -f gif - | gifsicle --optimize=3 --delay=3 > "$outFile" 
-	}
-	hub_create(){
-		printf "\e[1m"
-		git init
-		hub create
-		echo "# `basename $(pwd)`" > README.md
-		echo "# created by Jacob Menke" >> README.md
-		git add .
-		git commit -m "first commit"
-		git push --set-upstream origin master
-		printf "\e[0m"
+        ffmpeg -i "$1" -s "$res" -pix_fmt rgb24 -r 10 -f gif - | gifsicle --optimize=3 --delay=3 > "$outFile" 
+    }
+    hub_create(){
+        printf "\e[1m"
+        git init
+        hub create
+        echo "# `basename $(pwd)`" > README.md
+        echo "# created by Jacob Menke" >> README.md
+        git add .
+        git commit -m "first commit"
+        git push --set-upstream origin master
+        printf "\e[0m"
 
-	}
-	hub_delete(){
-		if [[ -z "$1" ]]; then
-			echo "need a REPO NAME" >&2
-			return 1
-		fi
-		REPO="$1"
-		out="$(curl -u menketechnologies -X "DELETE" https://api.github.com/repos/menketechnologies/"$REPO")"
+    }
+    hub_delete(){
+        if [[ -z "$1" ]]; then
+            echo "need a REPO NAME" >&2
+            return 1
+        fi
+        REPO="$1"
+        out="$(curl -u menketechnologies -X "DELETE" https://api.github.com/repos/menketechnologies/"$REPO")"
 
-		printf "\e[1m"
-		if [[ -z "$out" ]]; then
-			echo "Successful deletion of $REPO"
-		else
-			echo "Error in deletion of $REPO"
-			echo "$out"
-		fi
-		printf "\e[0m"
-	}
+        printf "\e[1m"
+        if [[ -z "$out" ]]; then
+            echo "Successful deletion of $REPO"
+        else
+            echo "Error in deletion of $REPO"
+            echo "$out"
+        fi
+        printf "\e[0m"
+    }
 
-	pstreeMonitor(){
-		bash $SCRIPTS/myWatchNoBlink.sh "pstree -g 2 -u $USER | sed s/$USER// | sed s@/.*/@@ | tail -75"
+    pstreeMonitor(){
+        bash $SCRIPTS/myWatchNoBlink.sh "pstree -g 2 -u $USER | sed s/$USER// | sed s@/.*/@@ | tail -75"
 
-	}
-	return2(){
-		exec 2> /dev/tty
-	}
-	color2(){
-		exec 2> >(redText.sh)
-	}
-	escapeRemove(){
-		while read INPUT; do
-			echo "$INPUT" | sed -e 's/\e\[.\{1,5\}m//g'
-		done
-	}
-	mp3(){
-		youtube-dl --extract-audio --audio-format mp3 "$1"
-	}
-	mp4(){
-		youtube-dl --no-playlist -f mp4 "$1"
-	}
-	prettyPrint(){
-		if [[ ! -z "$1" ]]; then
-			printf "\e[1m$1\e[0m\n"
-		else 
-			echo "Need one arg" >&2
-			return 1
-		fi
-	}
-	tac(){
-		sed '1!G;h;$!d' "$@"
-	}
+    }
+    return2(){
+        exec 2> /dev/tty
+    }
+    color2(){
+        exec 2> >(redText.sh)
+    }
+    escapeRemove(){
+        while read INPUT; do
+            echo "$INPUT" | sed -e 's/\e\[.\{1,5\}m//g'
+        done
+    }
+    mp3(){
+        youtube-dl --extract-audio --audio-format mp3 "$1"
+    }
+    mp4(){
+        youtube-dl --no-playlist -f mp4 "$1"
+    }
+    prettyPrint(){
+        if [[ ! -z "$1" ]]; then
+            printf "\e[1m$1\e[0m\n"
+        else 
+            echo "Need one arg" >&2
+            return 1
+        fi
+    }
+    tac(){
+        sed '1!G;h;$!d' "$@"
+    }
 
-	in(){
-		if [[ -z "$1" ]]; then
+    in(){
+        if [[ -z "$1" ]]; then
             if [[ ! -f "configure" ]];then
                 echo "Not in an installer dir..." >&2
                 return 1
             fi
-			./configure && make and sudo make install
-		else
-			tar xvfz "$1"
-			if [[ "$1" =~ .*.tar.gz ]];then
+            ./configure && make and sudo make install
+        else
+            tar xvfz "$1"
+            if [[ "$1" =~ .*.tar.gz ]];then
                 dir="${1%.tar.gz}"
             elif [[ "$1" =~ .*.tgz ]];then
                 dir="${1%.tgz}"
-			fi
+            fi
 
             cd "$dir"
-			./configure && make and sudo make install
-		fi
+            ./configure && make and sudo make install
+        fi
 
-	}
-	#}}}***********************************************************
+    }
+    #}}}***********************************************************
 
-	source "$HOME/.tokens.sh"
+    source "$HOME/.tokens.sh"
