96f11e083a8a6c803909c3fda886c66d59560828
MenkeTechnologies <linux.dev25@gmail.com>
2017-07-29 15:53:40 -0400

    changed keybinding to C-z for running directly from vim

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,22 +1,12 @@
 #!/usr/bin/env bash
 #created by JACOBMENKE at Wed Jun 28 21:45:18 EDT 2017 with /usr/local/bin/zsh
 
-
-pwd
-
-echo 'the dogs are here'
-
-ls /
-
-echo "my name si cool $(uname -a)"
+echo "coolio got back"
 
 
-pwd
 
 
-id
-
-say hello is my name and samantha is my game
 
 
-echo "$-"
+
+
