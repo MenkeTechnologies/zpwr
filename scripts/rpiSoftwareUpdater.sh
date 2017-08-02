f6edb2ca0a83a11f1f159e67f7dd47790e9c52f7
MenkeTechnologies <linux.dev25@gmail.com>
2017-08-02 00:12:12 -0400

    upgrade_oh_my_zsh workaround, directly running script

--- scripts/rpiSoftwareUpdater.sh
+++ scripts/rpiSoftwareUpdater.sh
@@ -40,7 +40,7 @@ prettyPrint "Updating Pathogen Plugins"
 gitRepoUpdater "$HOME/.vim/bundle"
 
 prettyPrint "Updating OhMyZsh"
-upgrade_oh_my_zsh
+cd "$HOME/.oh-my-zsh/tools" && bash "$HOME/.oh-my-zsh/tools/upgrade.sh"
 
 prettyPrint "Updating OhMyZsh Plugins"
 gitRepoUpdater "$HOME/.oh-my-zsh/custom/plugins"
