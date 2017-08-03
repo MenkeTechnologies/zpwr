688f4a31b2b813d61ad20ebda70532df93bcf76c
MenkeTechnologies <linux.dev25@gmail.com>
2017-08-03 12:56:43 -0400

    several mac only aliases to if block check uname

--- scripts/rsyncr.sh
+++ scripts/rsyncr.sh
@@ -7,10 +7,10 @@
 #####   Notes: 
 #}}}***********************************************************
 
-local="$1"
-remote="$2"
+localDIR="$1"
+rem="$2"
 host="r"
-rsync -vruhaE --progress --exclude="build.xml" -e ssh "$local" "$host":"$remote" --delete-after
+rsync -vruhaE --progress --exclude="build.xml" -e ssh "$localDIR" "$host":"$rem" --delete-after
 
 ssh "$host" '/home/pi/Downloads/apache-ant-1.9.7/bin/ant -buildfile "/home/pi/Downloads/apache-tomcat-8.5.5/apps/JH2_jmenke"'
 ssh "$host" '/home/pi/Downloads/apache-tomcat-8.5.5/bin/shutdown.sh'
