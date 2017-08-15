d8d379764a14fedc48715e6afd4f45341fc5ca91
MenkeTechnologies <linux.dev25@gmail.com>
2017-08-15 03:34:16 -0400

    supported for homebrew installed program log files in /usr/local/var/log/**

--- scripts/colorLogger.sh
+++ scripts/colorLogger.sh
@@ -24,7 +24,7 @@ if [[ $(uname) == Darwin ]]; then
     if [[ $weHaveCCZE == yes ]]; then
    $tailVersion -f /var/log/**/*.log /var/log/**/*.out \
        /var/log/cups/* "$HOME"/Library/Logs/**/*.log "$HOME"/Library/Logs/**/*.out \
-        /Library/Logs/**/*.log | ccze
+        /Library/Logs/**/*.log /usr/local/var/log/**/*.log | ccze
    else
    $tailVersion -f /var/log/**/*.log /var/log/**/*.out \
        /var/log/cups/* "$HOME"/Library/Logs/**/*.log "$HOME"/Library/Logs/**/*.out \
