c4c114a5fb382899252d9e1a5ffd98af04ad38dd
MenkeTechnologies <linux.dev25@gmail.com>
2017-09-17 14:07:46 -0400

    shell script with fswatch to allow long running task to run to completion before another file system event triggers

---
+++ scripts/fsWatchLongRunning.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Mon Jul 10 19:33:52 EDT 2017
+#####   Purpose: bash script to list files on file change
+#####   Notes: 
+#}}}***********************************************************
+DIR_WATCHING="$1"
+done=false
+while read -d "" event; do
+
+    echo "The event was $event"
+
+    # initial run
+    if [[ $done = false ]]; then
+    done=true
+    {
+        echo "starting long running task"
+        sleep 10
+        echo completing long running task like rsync
+    } &
+    pid=$!
+else
+    #subsequent runs
+    #dont execute commands is previous command still running
+    ps -ef | cut -c1-15 | grep -q "$pid" && echo "blocking next call..." || {
+
+    {
+        echo "starting long running task"
+        sleep 10
+        echo "completing long running task like rsync"
+    } &
+    pid=$!
+}
+
+fi
+
+
+
+done < <(fswatch -r -0 -E "$DIR_WATCHING" -e "/\.." )
