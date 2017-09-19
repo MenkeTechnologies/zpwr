7f1da60942e44a96fd4a1940c93f10b26fb45c9c
MenkeTechnologies <linux.dev25@gmail.com>
2017-09-19 03:38:37 -0400

    echo -e to printf and holding zone env var

--- scripts/upLoadDS.sh
+++ scripts/upLoadDS.sh
@@ -6,10 +6,10 @@
 #####   Purpose: bash script to upload file to NAS
 #####   Notes: 
 #}}}***********************************************************
-echo -e "\e[44;37mUploading $*"
+printf "\e[44;37mUploading $*\n"
 #loop through all arguments and upload with scp recursively to synology server
 for i in "$@"; do
-    scp -r -P $DS_PORT "$i" root@$IP:/volume1/homes/JAKENAS/holdingZone
+    scp -r -P "$DS_PORT" "$i" root@$IP:"$HOLDING_ZONE"
 done
 
-echo -e "Done\e[0m"
+printf "Done\n\e[0m"
