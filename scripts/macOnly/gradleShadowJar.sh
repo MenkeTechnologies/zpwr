8bac990faf2becfb11e6158fb5381bb2e8572d7a
MenkeTechnologies <linux.dev25@gmail.com>
2017-10-12 07:29:37 -0400

    header to gradle shadow jar script

--- scripts/macOnly/gradleShadowJar.sh
+++ scripts/macOnly/gradleShadowJar.sh
@@ -1,4 +1,12 @@
+#!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Thu Oct 12 07:28:22 EDT 2017
+#####   Purpose: bash script to facilitate running fat jars from gradle
+#####   Notes: run from shell script from intellij with bash support plugin
+#}}}***********************************************************
 
+#first arg is Project Name
 cd "$HOME/IdeaProjects/$1" && gradle shadowJar && java -jar build/libs/*.jar
-
 
