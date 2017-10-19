5475ec650ee22fdfb9c269f2bc751d6e165f0432
MenkeTechnologies <linux.dev25@gmail.com>
2017-10-19 00:06:18 -0400

    checking for log files in .vnc directory for linux

---
+++ scripts/test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Wed Oct 18 18:50:38 EDT 2017
+#####   Purpose: bash script to 
+#####   Notes: 
+#}}}***********************************************************
+
+exec >&2
+ls
+id
+pwd
+
+exec 2> >(blueText.sh)
