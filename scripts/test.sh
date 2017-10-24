82f3b48e97fb3d84fd82d0f9b99eba8a1c9c37bd
MenkeTechnologies <linux.dev25@gmail.com>
2017-10-23 21:29:42 -0400

    group-name completion zsh

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,7 +6,26 @@
 #####   Purpose: bash script to 
 #####   Notes: 
 #}}}***********************************************************
-
+#          .                                                      .
+#        .n                   .                 .                  n.
+#  .   .dP                  dP                   9b                 9b.    .
+# 4    qXb         .       dX                     Xb       .        dXp     t
+#dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+#9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+# 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+#  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+#    `9XXXXXXXXXXXP' `9XX'   DIE    `98v8P'  HUMAN   `XXP' `9XXXXXXXXXXXP'
+#        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+#                        )b.  .dbo.dP'`v'`9b.odb.  .dX(
+#                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+#                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+#                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+#                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP
+#                     `'      9XXXXXX(   )XXXXXXP      `'
+#                              XXXX X.`v'.X XXXX
+#                              XP^X'`b   d'`X^XX
+#                              X. 9  `   '  P )X
+#                              `b  `       '  d'
 
 x=5
 
@@ -20,12 +39,30 @@ say dogs
 
 echo "my name is dogs"
 
-
 x=50
-
 
 while (( x > 10)); do
     echo "its $x"
     let x--
 done
+
+
+for (( i = 0; i < 10000; i++ )); do
+    bash -c 'echo "$$"'
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
