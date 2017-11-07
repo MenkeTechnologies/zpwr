df9961350bae6f992ef3c03596de743cdb754be3
MenkeTechnologies <linux.dev25@gmail.com>
2017-11-06 21:44:56 -0500

    figlet Looper

---
+++ scripts/macOnly/figletLooper.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+ while [[ true ]]; do
+	 { { catme; perl -E 'say "dogs are $_" foreach @INC' | tr a-z A-Z | figlet -f stellar ; catme; catme | figlet; whoami | toilet | figlet -f ntgreek; echo $PATH ;catme ;} | cowsay -f daemon -W 120 | cowsay -f elephant-in-snake -W 150 | cowsay -f cheese -W 150 | cowsay -f dragon-and-cow -W 150; } | cowsay -f moofasa -W 150 | cowsay -f stegosaurus -W 150 | ponysay -W 150 | lolcat
+	sleep 1
+done
+
