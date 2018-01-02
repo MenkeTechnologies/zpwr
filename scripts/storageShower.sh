5aac798706f06000ba88f0194731d68d1d3902aa
MenkeTechnologies <linux.dev25@gmail.com>
2018-01-01 19:53:29 -0500

    added storage shower script

---
+++ scripts/storageShower.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Mon Jan  1 19:50:15 EST 2018
+#####   Purpose: bash script to 
+#####   Notes: 
+#}}}***********************************************************
+
+bash $SCRIPTS/myWatchMaintainEscapes.sh 'grc --colour=auto df -h'
