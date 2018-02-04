ec86b3b04c8acd57889e7ac5903a2c50d6f1d5c3
MenkeTechnologies <linux.dev25@gmail.com>
2018-02-04 15:19:17 -0500

    nz alias

--- .shell_aliases_functions.sh
+++ .shell_aliases_functions.sh
@@ -87,6 +87,7 @@ fi
 #{{{                          MARK:ALIASES
 #**********************************************************************
 #portable aliases
+alias nz='exec zsh'
 alias ll="clearList"
 alias la="clearList"
 alias l="clearList"
