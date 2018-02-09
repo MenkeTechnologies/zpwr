80b67ea295d1ca7035789f30bf6d84927a972a73
MenkeTechnologies <linux.dev25@gmail.com>
2018-02-08 23:22:41 -0500

    added help for storage shower

--- scripts/storageShower.sh
+++ scripts/storageShower.sh
@@ -7,6 +7,43 @@
 #####   Notes: 
 #}}}***********************************************************
 
+
+__ScriptVersion="version"
+
+#===  FUNCTION  ================================================================
+#         NAME:  usage
+#  DESCRIPTION:  Display usage information.
+#===============================================================================
+function usage ()
+{
+    echo "Usage :  $0 [options] lineCount RefreshTime(sec)
+
+    Options:
+    -h|help       Display this message
+    -v|version    Display script version"
+    
+
+}    # ----------  end of function usage  ----------
+
+#-----------------------------------------------------------------------
+#  Handle command line arguments
+#-----------------------------------------------------------------------
+
+while getopts ":hv" opt
+do
+  case $opt in
+
+    h|help     )  usage; exit 0   ;;
+
+    v|version  )  echo "$0 -- Version $__ScriptVersion"; exit 0   ;;
+
+    * )  echo -e "\n  Option does not exist : $OPTARG\n"
+          usage; exit 1   ;;
+
+  esac    # --- end of case ---
+done
+shift $(($OPTIND-1))
+
 if (( $# > 1)); then
     num="$1"
     shift
