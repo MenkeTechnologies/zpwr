29433337dca90350cb60d8b5394910db18ca47bf
MenkeTechnologies <linux.dev25@gmail.com>
2018-02-09 01:34:46 -0500

    send stderr and stdout to log file

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ set -v
 OS_TYPE="$(uname -s)"
 #resolve all symlinks
 INSTALLER_DIR="$(pwd -P)"
-exec 2> "$INSTALLER_DIR"/logfile.txt
+exec &> >(tee "$INSTALLER_DIR"/logfile.txt)
 
 #Dependencies
 # 1) vim 8.0
