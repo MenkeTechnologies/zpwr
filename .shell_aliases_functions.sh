c5ebe6c88428d4ec500beb18107644d282cd9b0f
MenkeTechnologies <linux.dev25@gmail.com>
2018-02-12 01:13:23 -0500

    grc aliases in better location in shell aliases file

--- .shell_aliases_functions.sh
+++ .shell_aliases_functions.sh
@@ -129,15 +129,17 @@ alias ka="killall"
 alias sin="./configure && make && sudo make install"
 alias curl='curl -fsSL'
 #Darwin specific aliases
-alias mount='grc --colour=auto -c "$HOME/conf.mount" mount'
+
 if [[ -f /usr/local/share/grc/conf.gls ]];then
     alias lr='grc -c /usr/local/share/grc/conf.gls gls -iAlhFR --color=always'
+alias mount='grc --colour=auto -c "$HOME/conf.mount" mount'
+    alias ifconfig='grc --colour=auto /sbin/ifconfig'
 elif [[ -f /usr/share/grc/conf.gls ]];then
     alias lr='grc -c /usr/share/grc/conf.gls ls -iAlhFR --color=always'
+alias mount='grc --colour=auto -c "$HOME/conf.mount" mount'
 fi
 if [[ "$(uname)" == "Darwin" ]]; then
     #statements
-    alias ifconfig='grc --colour=auto /sbin/ifconfig'
     alias spd="du -csh {.[^.]*,..?*} * 2> /dev/null | gsort -h"
     alias cpu="top -o cpu"
     alias mem="top -o mem"
