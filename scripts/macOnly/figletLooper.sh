c5cffb55c4713a59b7bce8c2cd6169070d68cc17
MenkeTechnologies <linux.dev25@gmail.com>
2018-03-30 15:57:08 -0400

    fix for macvim error

--- scripts/macOnly/figletLooper.sh
+++ scripts/macOnly/figletLooper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
  while [[ true ]]; do
-	 { { catme; perl -E 'say "dogs are $_" foreach @INC' | tr a-z A-Z | figlet -f alligator ; catme; catme | figlet; whoami | toilet | figlet -f ntgreek; echo $PATH ;catme ;} | cowsay -f daemon -W 120 | cowsay -f elephant-in-snake -W 150 | cowsay -f cheese -W 150 | cowsay -f dragon-and-cow -W 150; } | cowsay -f moofasa -W 150 | cowsay -f stegosaurus -W 150 | ponysay -W 150 | lolcat
+	 { { cat; perl -E 'say "dogs are $_" foreach @INC' | tr a-z A-Z | figlet -f alligator ; cat; cat | figlet; whoami | toilet | figlet -f ntgreek; echo $PATH ;cat ;} | cowsay -f daemon -W 120 | cowsay -f elephant-in-snake -W 150 | cowsay -f cheese -W 150 | cowsay -f dragon-and-cow -W 150; } | cowsay -f moofasa -W 150 | cowsay -f stegosaurus -W 150 | ponysay -W 150 | lolcat
 	sleep 1
 done
 
