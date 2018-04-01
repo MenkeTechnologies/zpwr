8ed26ad0a949c92aac1a09077f3ab1f264a5f51f
MenkeTechnologies <linux.dev25@gmail.com>
2018-04-01 18:24:12 -0400

    connect to tmux auto

---
+++ scripts/forever.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Sun Apr  1 18:12:27 EDT 2018
+#####   Purpose: bash script to 
+#####   Notes: 
+#}}}***********************************************************
+
+while [[ 1 ]]; do
+    eval "$1"
+done
