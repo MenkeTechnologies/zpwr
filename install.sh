56185dce6de5ba46ad8231b632666fd32dc9c287
MenkeTechnologies <linux.dev25@gmail.com>
2018-04-06 15:46:23 -0400

    libxml2-devel libxslt-devel dependencies

--- install.sh
+++ install.sh
@@ -68,7 +68,7 @@ addDependenciesArch(){
 
 addDependenciesSuse(){
     dependencies_ary+=(mariadb postgresql-server fortune python3-devel python-devel ruby-devel openssl-devel \
-        python3-pip curl libffi-devel grc libpcap-devel the_silver_searcher kernel-devel gcc) 
+        python3-pip curl libffi-devel grc libpcap-devel the_silver_searcher kernel-devel gcc libxml2-devel libxslt-devel) 
 
 }
 
