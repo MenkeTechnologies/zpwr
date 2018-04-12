772a7775d75f0c2972bc1ac288816760ad93fcb5
MenkeTechnologies <linux.dev25@gmail.com>
2018-04-12 00:07:31 -0400

    added gitRemoteFetcher

---
+++ scripts/gitRemoteFetcher.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Thu Apr 12 00:02:45 EDT 2018
+#####   Purpose: bash script to 
+#####   Notes: 
+#}}}***********************************************************
+
+while [[ 1 ]]; do
+
+    git fetch origin
+    output=$(git log HEAD..origin/master --oneline)
+
+    if [[  ! -z "$output" ]] ; then
+        echo "We have change to $(git remote)" >> "$LOGFILE"
+        git merge origin/master
+
+		cp .shell_aliases_functions.sh "$HOME"
+		cp .zshrc "$HOME"
+		cp .vimrc "$HOME"
+		cp .tmux.conf "$HOME"
+		cp conf.gls "$HOME"
+		cp conf.df "$HOME"
+		cp conf.ifconfig "$HOME"
+		cp grc.zsh "$HOME"
+		cp .inputrc "$HOME"
+		cp -R .tmux/* "$HOME/.tmux"
+		cp -f scripts/* "$SCRIPTS"
+    else
+        echo "No change to $(git remote)" >> "$LOGFILE"
+          :
+    fi
+
+    sleep 10
+done
+
