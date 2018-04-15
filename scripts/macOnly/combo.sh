c0f3307ff12caa947182432e4bb078c8c0c9fcd1
MenkeTechnologies <linux.dev25@gmail.com>
2018-04-15 15:39:27 -0400

    final sed fixes

--- scripts/macOnly/combo.sh
+++ scripts/macOnly/combo.sh
@@ -3,25 +3,25 @@
 #**************************************************************
 #####   Author: JACOBMENKE
 #####   Date: Thu Nov  9 04:18:33 EST 2017
-#####   Purpose: bash script to 
-#####   Notes: 
+#####   Purpose: bash script to
+#####   Notes:
 #}}}***********************************************************
 o="$(cat)"
 randomFigletFontScript="$SCRIPTS/macOnly/figletRandomFontOnce.sh"
-randomCowsayCowScript="$SCRIPTS/macOnly/randomCow.sh" 
+randomCowsayCowScript="$SCRIPTS/macOnly/randomCow.sh"
 
 echo "$o" | {
     catme
-    catme | shelobsay 
+    catme | shelobsay
     echo "we got $o"
     echo "tommy was here @ $o"
     echo "bad peeeeps are "
-    perl -E "say foreach @INC;say;say 'tommy tribe@ $o';say \"$^O baby.\"."baby";" | sed 's/.*/<&>/g' | fold -w 50 | "$randomCowsayCowScript" 120
+    perl -E "say foreach @INC;say;say 'tommy tribe@ $o';say \"$^O baby.\"."baby";" | sed 's@.*@<&>@g' | fold -w 50 | "$randomCowsayCowScript" 120
     echo
     perl -E 'say "perl"' | "$randomFigletFontScript"
     echo
-    cat /etc/passwd | perl -nE 'print "dogs gone wild $_"' | head | sed 's/./<&>/g'| fold -w 50 | "$randomCowsayCowScript" 100
-} | sed 's/./&/g' | {
+    cat /etc/passwd | perl -nE 'print "dogs gone wild $_"' | head | sed 's@.@<&>@g'| fold -w 50 | "$randomCowsayCowScript" 100
+} | sed 's@.@&@g' | {
     cat > /dev/stdout
     echo "SCRIPTING" | "$randomFigletFontScript"
     {
@@ -31,7 +31,7 @@ echo "$o" | {
     x="$(python -c 'print("trina"*5)')"
     ruby -e "10.times do |x| puts \"dogs are cool $x #{x}\" end" | yosay
     perl -E 'say "TCL IN THE HOUSE",a..z,-235..235' | fold -w 10 | pr -t -3
-    perl -E 'say "perl ftw"' | toilet 
+    perl -E 'say "perl ftw"' | toilet
     node -e 'console.log(process)'  | head | "$randomCowsayCowScript" 80
     x=$(date | cut -c1-10)
     #echo "set x 55; puts \"we got \$x $x\"" | tclsh | "$randomFigletFontScript"
