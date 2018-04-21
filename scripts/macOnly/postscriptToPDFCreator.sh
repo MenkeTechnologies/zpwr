d99e44b8d52c357d53bbaff209900225c5161f33
MenkeTechnologies <linux.dev25@gmail.com>
2018-04-21 17:13:11 -0400

    added vim > postscript > pdf script

---
+++ scripts/macOnly/postscriptToPDFCreator.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Sat Apr 21 17:07:54 EDT 2018
+#####   Purpose: bash script to
+#####   Notes:
+#}}}***********************************************************
+
+
+for file in "$@" ; do
+    newfile=${file%.*}
+    vim -c 'hardcopy > '"$newfile.ps" -c quitall "$file"
+done
+
+cat *.ps | open -fa Preview
+
+rm -f *.ps
