d7701447fdeee80c7c31d740c20e491234c0249b
MenkeTechnologies <linux.dev25@gmail.com>
2018-05-29 14:43:41 -0400

    better git remote repo, works for ssh and http repos on github

--- scripts/gitRemoteRepoInformation.sh
+++ scripts/gitRemoteRepoInformation.sh
@@ -11,12 +11,19 @@ for directory; do
     if [[ -d "$directory" ]]; then
         cd "$directory" && {
             [[ -d .git ]] && {
-                line="$(git remote -v 2>/dev/null)" && {
-                    user="$(echo $line | awk -F'/' '{print $4}')"
-                    repo="$(echo $line | awk -F'/' '{print $5}' | awk '{print $1}')"
+                line="$(git remote -v 2>/dev/null | sed 1q)" && {
+                    echo "$line" | grep -q 'git@' && {
+                        #ssh
+                        user="$(echo $line | awk -F':' '{print $2}' | awk -F'/' '{print $1}')"
+                        repo="$(echo $line | awk -F'/' '{print $2}' | awk '{print $1}')"
+                    } || {
+                        #http
+                        user="$(echo $line | awk -F'/' '{print $4}')"
+                        repo="$(echo $line | awk -F'/' '{print $5}' | awk '{print $1}')"
+                    }
                     echo "$user/${repo%%.git*}"
                 }
+            }
         }
-    }
-fi
+    fi
 done
