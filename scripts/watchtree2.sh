88f47b4efba2f466b291f3ae7bcd49e8f895de47
MenkeTechnologies <linux.dev25@gmail.com>
2018-06-20 11:49:15 -0400

    perl script for catting all args and piping to less

--- scripts/watchtree2.sh
+++ scripts/watchtree2.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/env bash
-#{{{                    MARK:Header
+#{{{ MARK:Header
 #**************************************************************
-#####   Author: JACOBMENKE
-#####   Date: Mon Jul 10 19:36:58 EDT 2017
-#####   Purpose: bash script to monitor tree of processes
-#####   Notes:
+##### Author: JACOBMENKE
+##### Date: Mon Jul 10 19:36:58 EDT 2017
+##### Purpose: bash script to monitor tree of processes
+##### Notes:
 #}}}***********************************************************
-
 bash myWatchNoBlink.sh -b 'pstree -g 3 -u '"$(whoami)"' | sed s@'"$(whoami)"'@@ | sed s@/.*/@@ | tail -75' 1
-
-
-
-
-
-
-
-
-
-
-
