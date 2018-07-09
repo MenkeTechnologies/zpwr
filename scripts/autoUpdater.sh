6fc91ee9e2cf4b4796af5d8d748c4ef96d5e995a
MenkeTechnologies <linux.dev25@gmail.com>
2018-07-09 14:44:01 -0400

    autoUpdater script

---
+++ scripts/autoUpdater.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+#####   Author: JACOBMENKE
+#####   Date: Mon Jul  9 14:42:39 EDT 2018
+#####   Purpose: bash script to
+#####   Notes:
+#}}}***********************************************************
+
+while [[ 1 ]]; do
+    bash -l updater.sh
+    sleep $((3600 * 24))
+
+done
