0553eee91835c763458658b0be32369c039c8014
MenkeTechnologies <linux.dev25@gmail.com>
2018-07-14 00:10:48 -0400

    updated minifier to not touch leading white space

--- scripts/transfer.sh
+++ scripts/transfer.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
-#{{{                    MARK:Header
+#{{{                   MARK:Header
 #**************************************************************
 #####   Author: JACOBMENKE
 #####   Date: Mon Jul 10 19:29:29 EDT 2017
 #####   Purpose: bash script to transfer file through scp
 #####   Notes:
 #}}}***********************************************************
-
 (( $# < 1 )) && echo "First Arg is the file to transfer" >&2 && exit 1
-
 transferFile="$1"
-
 #escape $HOME to prevent expansion on original host
 scp -r "$transferFile" 'r2:$HOME/Desktop'
