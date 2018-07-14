54b75eb195fdfbebea270675b3a17981aa115549
MenkeTechnologies <linux.dev25@gmail.com>
2018-07-14 00:00:36 -0400

    minified autoUpdater.sh

--- scripts/autoUpdater.sh
+++ scripts/autoUpdater.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
-#{{{                    MARK:Header
+#{{{ MARK:Header
 #**************************************************************
-#####   Author: JACOBMENKE
-#####   Date: Mon Jul  9 14:42:39 EDT 2018
-#####   Purpose: bash script to
-#####   Notes:
+##### Author: JACOBMENKE
+##### Date: Mon Jul  9 14:42:39 EDT 2018
+##### Purpose: bash script to
+##### Notes:
 #}}}***********************************************************
-
 while [[ 1 ]]; do
     echo "$(date) Updating Software" >> "$LOGFILE"
     bash -l updater.sh -e
     oldtime=$(date +"%s")
-
     while [[ 1 ]]; do
         sleep $((3*60))
         newtime=$(Date +"%s")
         timediff=$(($newtime-$oldtime))
         echo "Time diff $timediff for $(date) new:$newtime old: $oldtime " >> "$LOGFILE"
-        
         if (( $timediff > $((3600*24)) )); then
             break
         fi
