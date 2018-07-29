983d3aa56cad83ab33085dd75ccafbfc83c6a49b
MenkeTechnologies <linux.dev25@gmail.com>
2018-07-29 16:45:31 -0400

    better space remover

--- .shell_aliases_functions.sh
+++ .shell_aliases_functions.sh
@@ -891,7 +891,10 @@ mycurl(){
 
 
 perlremovespaces(){
-    perl -pi -e 's@\s+$@\n@g; s@\x09$@    @g;s@\x20@ @g; s@^s*\n$@@; s@(\S)[\x20]{2,}@$1\x20@' "$@"
+    for file in "$@";do
+        echo "Working on $file"
+        perl -pi -e 's@\s+$@\n@g; s@\x09$@    @g;s@\x20@ @g; s@^s*\n$@@; s@(\S)[\x20]{2,}@$1\x20@' "$file"
+    done
 }
 
 pirun(){
