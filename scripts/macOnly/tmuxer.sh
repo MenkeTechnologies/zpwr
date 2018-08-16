c53f6494d6cc20dccfa01899db777abcad596576
MenkeTechnologies <linux.dev25@gmail.com>
2018-08-16 03:02:20 -0400

    rename gds fxn so does not conflict with git diff --staged

---
+++ scripts/macOnly/tmuxer.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#{{{ MARK:Header
+#**************************************************************
+##### Author: WIZARD
+##### Date: Sun Aug 12 19:57:49 EDT 2018
+##### Purpose: bash script to
+##### Notes:
+#}}}***********************************************************
+
+PATH="/usr/local/bin:$PATH"
+
+tmux send-keys -t main "id" C-m
