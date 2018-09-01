139807152eaa493367d4bbd76222e623cecc22f7
MenkeTechnologies <linux.dev25@gmail.com>
2018-08-31 23:52:32 -0400

    pingordie

--- scripts/pingordie.sh
+++ scripts/pingordie.sh
@@ -7,6 +7,39 @@
 ##### Notes:
 #}}}***********************************************************
 
+gateway_ip='8.8.8.8'
+
 while true; do
-    ping -c 1 8.8.8.8 || sudo reboot
+    network_check_tries=0
+
+    network_check_threshold=5
+
+    restart_wlan0() {
+        echo "Network was not working for the previous $network_check_tries checks."
+        echo "Restarting wlan0"
+        ifdown wlan0
+        sleep 5
+        ifup --force wlan0
+        sleep 60
+        host_status=$(fping $gateway_ip)
+        echo "$host_status" | grep -iq alive || sudo reboot
+    }
+
+    while (( $network_check_tries < $network_check_threshold )); do
+
+        host_status=$(fping $gateway_ip)
+
+        (($network_check_tries++))
+        
+        echo "$host_status" | grep -iq alive && {
+            echo "Network is working correctly" && exit 0
+        } || {
+            echo "Network is down, failed check number $network_check_tries of $network_check_threshold"
+        }
+        
+        (($network_check_tries >= $network_check_threshold)) && restart_wlan0
+
+        sleep 5
+    done
 done
+
