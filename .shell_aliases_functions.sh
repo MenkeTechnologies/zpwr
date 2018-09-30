179fe7e9d275bdb43dd06d24c8a5f204b6a75504
MenkeTechnologies <linux.dev25@gmail.com>
2018-09-30 15:48:35 -0400

    \d doesnt work do use [0-9]

--- .shell_aliases_functions.sh
+++ .shell_aliases_functions.sh
@@ -320,7 +320,7 @@ urlsafe(){
 cgh(){
     [[ -z "$1" ]] && user="$GITHUB_ACCOUNT" || user="$1"
     curl -s "https://github.com/$user" | \
-        command grep -E '\d contributions' | sed 1q | tr -s ' '
+        command grep -E '[0-9] contributions' | sed 1q | tr -s ' '
 }
 
 upload(){
