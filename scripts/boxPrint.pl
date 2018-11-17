618ea953f48874bd71d284cec7082594e1e884d4
MenkeTechnologies <linux.dev25@gmail.com>
2018-11-17 12:08:28 -0500

    added space to shell fxns file

--- scripts/boxPrint.pl
+++ scripts/boxPrint.pl
@@ -3,13 +3,12 @@
 #**************************************************************
 ##### Author: WIZARD
 ##### Date: Fri Nov 16 21:18:20 EST 2018
-##### Purpose: bash script to
+##### Purpose: perl script to print boxes around text
 ##### Notes:
 #}}}***********************************************************
 
 use strict;
 use warnings;
-
 use feature 'say';
 
 my $width = 70;
@@ -21,14 +20,15 @@ my $spaceChar = ' ';
 my $sidelen = (($width - $len - $spacerlen * 2) + 1) / 2;
 
 my $proposedTextLength = $len + $spacerlen * 2 + 4;
+my $maxTextLength = $width - (($spacerlen * 2) + 4);
 
-if ($proposedTextLength > $width) {
+if ($proposedTextLength > $maxTextLength) {
 
     say "too long at $len";
-    say "max text length is $proposedTextLength";
+    say "max text length is $maxTextLength";
 
     my $lineCounter = 0;
-    while ($len > $proposedTextLength) {
+    while ($len > $maxTextLength) {
         $len /= 2;
         ++$lineCounter;
     }
@@ -42,6 +42,7 @@ if ($proposedTextLength > $width) {
         # print $str[$numchars*$i..$numchars*($i+1)];
         print $str;
         print $spaceChar x $spacerlen;
+        print "\n";
     }
 
     say $boxesChar x $width;
