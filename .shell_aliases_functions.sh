400846582162e82ae5d694efc754e2deecf42c5f
MenkeTechnologies <linux.dev25@gmail.com>
2018-12-20 17:40:15 -0500

    alias ag

--- .shell_aliases_functions.sh
+++ .shell_aliases_functions.sh
@@ -1161,7 +1161,7 @@ c(){
 set +x
 }
 fz(){
-    ag '.' --color| fzf --delimiter : --nth 3.. --reverse --border --prompt='-->>> ' --preview '[[ -f $(cut -d: -f1 <<< {}) ]] && pygmentize -g $(cut -d: -f1 <<< {}) \
+    command ag '.' --color| fzf --delimiter : --nth 3.. --reverse --border --prompt='-->>> ' --preview '[[ -f $(cut -d: -f1 <<< {}) ]] && pygmentize -g $(cut -d: -f1 <<< {}) \
         2>/dev/null | cat -n | sed -n "$(cut -d: -f2 <<< {}),\$p" || stat $(cut -d: -f1 <<< {}) | fold -80 | head -500' --ansi | cut -d ':' -f1
 }
 
