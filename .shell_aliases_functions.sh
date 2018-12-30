871d89706cd4cb92d4f07bdc19753b8689f948e8
MenkeTechnologies <linux.dev25@gmail.com>
2018-12-30 18:11:08 -0500

    new reveal function

--- .shell_aliases_functions.sh
+++ .shell_aliases_functions.sh
@@ -911,10 +911,30 @@ jetbrainsWorkspaceEdit(){
     sleep 1
     done
 }
-reveal(){
-    [[ ! -d .git ]] && echo "Not git dir" >&2 && return 1
-    open "$(git remote -v | command grep fetch | awk '{print $2}' | sed 's@.git$@@')"
+
+reveal() {
+    OS="$(uname -s)"
+    case "$OS" in
+        Linux*)     open_cmd=xdg-open;;
+        Darwin*)    open_cmd=open;;
+        CYGWIN*)    open_cmd=cygstart;;
+        MINGW*)     open_cmd=start;;
+        *)          open_cmd="UNKNOWN:$OS"
+        echo "Your OS: $OS is unsupported..." >&2 && return 2;;
+    esac
+
+    [[ ! -d .git ]] && echo "Not a git directory" >&2 && return 1
+
+    git remote -v | \grep "$1" | \grep 'heroku' | \grep fetch | \grep -o -E ':.*' | \
+    cut -c 19- | awk '{print $1}' | sed 's@.git$@@' | \
+    xargs -I {} "$open_cmd" https://dashboard.heroku.com/apps/{} https://{}.herokuapp.com
+
+  {
+    git remote -v | \grep "$1" | \grep '@'  | \grep -o -E '@.*' | cut -c 2-;
+    git remote -v | \grep "$1" | \grep '//' | \grep -o -E ':.*' | cut -c 4- | \grep -v 'heroku';
+  } | \grep fetch | sed 's@:/\\@@g' | awk '{print $1}' | sed 's@.git$@@' | xargs -I {} "$open_cmd" https://www.{}
 }
+
 
 getrc(){
     REPO_NAME="customTerminalInstaller"
