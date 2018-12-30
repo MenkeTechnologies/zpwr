f44be99d3a3cd1fb4bd9c4d84e1ca896a50b1b96
MenkeTechnologies <linux.dev25@gmail.com>
2018-12-30 18:11:08 -0500

    new reveal function

--- scripts/redText.sh
+++ scripts/redText.sh
@@ -11,6 +11,6 @@ RED='\e[31;1m'
 RESET='\e[0m'
 
 #loop through stdin and add escape sequences at head and tail of each line
-while read input; do
-    echo -e "${RED}$input${RESET}"
+while read; do
+    echo -e "${RED}$REPLY${RESET}"
 done
