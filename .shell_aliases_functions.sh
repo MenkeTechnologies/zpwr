8b5cf30696a05b711006bf6c626d635bfeecdcc3
MenkeTechnologies <linux.dev25@gmail.com>
2019-01-02 22:53:38 -0500

    zsh fxn into zshrc

--- .shell_aliases_functions.sh
+++ .shell_aliases_functions.sh
@@ -945,14 +945,10 @@ reveal() {
   } | command grep fetch | command sed 's@:/\\@@g' | command awk '{print $1}' | sed 's@.git$@@' | command xargs -I {} "$open_cmd" https://www.{}
 }
 
-revealRecurse(){
-    for i in **/*(/); do ( builtin cd $i && reveal 2>/dev/null); done
-}
 
 openMyGH(){
     open "https://github.com/$GITHUB_ACCOUNT"
 }
-
 
 getrc(){
     REPO_NAME="customTerminalInstaller"
