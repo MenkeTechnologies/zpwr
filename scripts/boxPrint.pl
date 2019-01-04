8b7bd2caac3853d267cebdd9c4a36e7923922d37
MenkeTechnologies <linux.dev25@gmail.com>
2019-01-03 22:55:53 -0500

    better boxPrint.pl

--- scripts/boxPrint.pl
+++ scripts/boxPrint.pl
@@ -10,55 +10,74 @@
 use strict;
 use warnings;
 use feature 'say';
+use POSIX;
 
 my $width = 70;
-my $str = $ARGV[0];
-my $len = length $str;
-my $spacerlen = 2;
+my $str = '';
+
+if (scalar @ARGV > 0) {
+    $str .= "$_ " for @ARGV;
+} else {
+    while (<STDIN>) {
+        chomp $_;
+        $ ~= s@\s+@ @g;
+        $str .= $_;
+
+    }
+}
+
+my $inputStringLength = length $str;
+
+my $spacerLength = 2;
 my $boxesChar = '/';
 my $spaceChar = ' ';
-my $sidelen = (($width - $len - $spacerlen * 2) + 1) / 2;
+my $sideCharacterLength = (($width - $inputStringLength - $spacerLength * 2) + 1) / 2;
 
-my $proposedTextLength = $len + $spacerlen * 2 + 4;
-my $maxTextLength = $width - (($spacerlen * 2) + 4);
+my $proposedTextLength = $inputStringLength + $spacerLength * 2 + 4;
+my $maxTextLength = $width - (($spacerLength * 2) + 4);
 
 if ($proposedTextLength > $maxTextLength) {
 
-    say "too long at $len";
-    say "max text length is $maxTextLength";
+    my $lineCounter = int($inputStringLength / $maxTextLength) + 1;
+    my $charactersPerLine = int($inputStringLength / $lineCounter);
+    my $numSideCharLength = int(int($width - $charactersPerLine - 4 )/2);
 
-    my $lineCounter = 0;
-    while ($len > $maxTextLength) {
-        $len /= 2;
-        ++$lineCounter;
-    }
-    my $numchars = $len / $lineCounter;
+    # say STDOUT '___________$lineCounter = ' . $lineCounter . '___________';
+    # say STDOUT '___________$charactersPerLine = ' . $charactersPerLine . '___________';
+    # say STDOUT '___________$numSideCharLength = ' . $numSideCharLength . '___________';
+
 
     say $boxesChar x $width;
 
-    for (my $i=0; $i<$lineCounter; $i++) {
-        print $boxesChar x $sidelen;
-        print $spaceChar x $spacerlen;
-        # print $str[$numchars*$i..$numchars*($i+1)];
-        print $str;
-        print $spaceChar x $spacerlen;
+    for (my $i = 0; $i < $lineCounter; $i++) {
+
+        my $startIndex = $charactersPerLine * $i;
+
+        print $boxesChar x $numSideCharLength;
+        print $spaceChar x $spacerLength;
+
+        print substr $str, $startIndex, $charactersPerLine;
+
+        print $spaceChar x $spacerLength;
+        print $boxesChar x $numSideCharLength;
         print "\n";
     }
 
     say $boxesChar x $width;
 
-} else {
-    my $side2len = $sidelen;
+}
+else {
+    my $side2len = $sideCharacterLength;
 
-    if ($len % 2 == 1) {
+    if ($inputStringLength % 2 == 1) {
         --$side2len;
     }
 
     say $boxesChar x $width;
-    print $boxesChar x $sidelen;
-    print $spaceChar x $spacerlen;
+    print $boxesChar x $sideCharacterLength;
+    print $spaceChar x $spacerLength;
     print $str;
-    print $spaceChar x $spacerlen;
+    print $spaceChar x $spacerLength;
     say $boxesChar x $side2len;
     say $boxesChar x $width;
 
