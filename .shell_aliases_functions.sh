54042e1b16a11f8573457b2d3095c304210bf6b6
MenkeTechnologies <linux.dev25@gmail.com>
2019-02-05 19:10:35 -0500

    exists jenv to not crash

--- .shell_aliases_functions.sh
+++ .shell_aliases_functions.sh
@@ -64,25 +64,25 @@ echo "$PATH" | grep -iq shellScripts || {
 #{{{                           MARK:HOMES
 #**********************************************************************
     [[ "$(uname)" == Darwin ]] && {
-        export PATH="/Users/wizard/.jenv/shims:${PATH}"
-        export JENV_SHELL=zsh
-        export JENV_LOADED=1
-        unset JAVA_HOME
-        source '/usr/local/Cellar/jenv/0.5.1/libexec/libexec/../completions/jenv.zsh'
-        jenv rehash 2>/dev/null
-        jenv() {
-        typeset command
-        command="$1"
-        if [ "$#" -gt 0 ]; then
-            shift
-        fi
-        case "$command" in
-        enable-plugin|rehash|shell|shell-options)
-            eval `jenv "sh-$command" "$@"`;;
-        *)
-            command jenv "$command" "$@";;
-        esac
-        }
+        exists jenv && {
+            export PATH="/Users/wizard/.jenv/shims:${PATH}"
+            export JENV_SHELL=zsh
+            export JENV_LOADED=1
+            unset JAVA_HOME
+            source '/usr/local/Cellar/jenv/0.5.1/libexec/libexec/../completions/jenv.zsh'
+            jenv rehash 2>/dev/null
+            jenv() {
+                typeset command
+                command="$1"
+                (("$#" > 0 )) && shift
+                case "$command" in
+                enable-plugin|rehash|shell|shell-options)
+                    eval `jenv "sh-$command" "$@"`;;
+                *)
+                    command jenv "$command" "$@";;
+                esac
+            }
+    }
         export HOMEBREW_HOME='/usr/local/Cellar'
         export HOMEBREW_OPT_HOME='/usr/local/opt'
         export GROOVY_LIB="$HOMEBREW_OPT_HOME/groovy"
