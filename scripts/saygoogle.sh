2fc47f451fc6cfb58e1f2a4ad5fb246f57e59870
MenkeTechnologies <linux.dev25@gmail.com>
2019-03-02 14:26:52 -0500

    say google shell script

---
+++ scripts/saygoogle.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#{{{ MARK:Header
+#**************************************************************
+##### Author: PI
+##### Date: Sun Aug 19 23:45:15 EDT 2018
+##### Purpose: bash script to
+##### Notes: use google's text to speech service
+#}}}***********************************************************
+local IFS=+
+/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols \
+"http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"
