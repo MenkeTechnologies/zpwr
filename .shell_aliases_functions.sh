0da7166850b3105c25272648ac7418e4c19ef68f
MenkeTechnologies <linux.dev25@gmail.com>
2019-04-02 00:04:02 -0400

    restartpoll supports root user

--- .shell_aliases_functions.sh
+++ .shell_aliases_functions.sh
@@ -426,7 +426,11 @@ r(){
         test -d "$src_dir" || { echo "$src_dir does not exists." >&2 && return 1; }
         test -d "/etc/systemd/system" || { echo "/etc/systemd/system does not exists. Is systemd installed?" >&2 && return 1; }
         git -C "$src_dir" pull
-        perl -i -pe "s@pi@$USER@g" "$src_dir/poll.service"
+        if [[ $UID != 0 ]]; then
+            perl -i -pe "s@pi@$USER@g" "$src_dir/poll.service"
+        else
+            perl -i -pe "s@/home/pi@$USER@g" "$src_dir/poll.service"
+        fi
         sudo cp "$HOME/forkedRepos/$REPO_NAME/poll.service" /etc/systemd/system
         sudo systemctl daemon-reload
         sudo systemctl restart poll.service
