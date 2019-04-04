54fcb0947639a3eec5da557db2664bd5bc7201be
MenkeTechnologies <linux.dev25@gmail.com>
2019-04-04 10:27:06 -0400

    messages

--- .shell_aliases_functions.sh
+++ .shell_aliases_functions.sh
@@ -1510,10 +1510,19 @@ se(){
 }
 
 createLearningCollection(){
-    if ! echo "select * from information_schema.tables" | mysql -u root -p | \grep --color=always -q "$TABLE_NAME";then
-       echo  "create schema $SCHEMA_NAME if not exists" | mysql -u root -p
+    prettyPrint "Creating$DELIMITER_CHAR $SCHEMA_NAME.$TABLE_NAME$DELIMITER_CHAR"
+    if [[ -z "$1" ]]; then
+        if ! echo "select * from information_schema.tables" | mysql -u root -p | \grep --color=always -q "$TABLE_NAME";then
+        echo  "create schema $SCHEMA_NAME if not exists" | mysql -u root -p
 
-        echo 'CREATE TABLE `'"$TABLE_NAME"'` ( `category` varchar(20) DEFAULT NULL, `learning` varchar(200) DEFAULT NULL,`dateAdded` datetime DEFAULT NULL, `id` int(11) NOT NULL AUTO_INCREMENT,  PRIMARY KEY (`id`), KEY `'"$TABLE_NAME"'learning_index` (`learning`))' | mysql -u root -D "$SCHEMA_NAME" -p
+            echo 'CREATE TABLE `'"$TABLE_NAME"'` ( `category` varchar(20) DEFAULT NULL, `learning` varchar(200) DEFAULT NULL,`dateAdded` datetime DEFAULT NULL, `id` int(11) NOT NULL AUTO_INCREMENT,  PRIMARY KEY (`id`), KEY `'"$TABLE_NAME"'learning_index` (`learning`))' | mysql -u root -D "$SCHEMA_NAME" -p
+        fi
+    else
+        if ! echo "select * from information_schema.tables" | mysql -u root | \grep --color=always -q "$TABLE_NAME";then
+        echo  "create schema $SCHEMA_NAME if not exists" | mysql -u root
+
+            echo 'CREATE TABLE `'"$TABLE_NAME"'` ( `category` varchar(20) DEFAULT NULL, `learning` varchar(200) DEFAULT NULL,`dateAdded` datetime DEFAULT NULL, `id` int(11) NOT NULL AUTO_INCREMENT,  PRIMARY KEY (`id`), KEY `'"$TABLE_NAME"'learning_index` (`learning`))' | mysql -u root -D "$SCHEMA_NAME"
+        fi
     fi
 }
 
