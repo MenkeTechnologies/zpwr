add527368b87cd5b51adc5a43343503dcde2b741
MenkeTechnologies <linux.dev25@gmail.com>
2019-04-20 10:52:09 -0400

    ycm separate

---
+++ ycm_install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#{{{ MARK:Header
+#**************************************************************
+##### Author: WIZARD
+##### Date: Sat Apr 20 10:49:16 EDT 2019
+##### Purpose: bash script to
+##### Notes:
+#}}}***********************************************************
+
+prettyPrint "Installing YouCompleteMe"
+mkdir -p "$HOME/.vim/bundle"
+
+cd "$HOME/.vim/bundle" || { echo "no .vim/bundle dir" >&2; exit1; }
+git clone https://github.com/Valloric/YouCompleteMe.git
+cd "$HOME/.vim/bundle/YouCompleteMe" && {
+    git submodule update --init --recursive
+    #need greater than 3GB RAM for compiling
+    YCM_CORES=1 ./install.py --clang-completer
+}
+
+
