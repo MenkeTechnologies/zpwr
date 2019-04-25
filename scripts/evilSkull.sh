dbfcb257ffd7d5e8d99e6dc4640e6333aedb0192
MenkeTechnologies <linux.dev25@gmail.com>
2019-04-24 23:42:26 -0400

    fill evil skull

--- scripts/evilSkull.sh
+++ scripts/evilSkull.sh
@@ -9,26 +9,25 @@
 
 {
 cat <<\EOF
-# . .
-# .n .                 .                  n.
-# . .dP                  dP                   9b                 9b.    .
-# 4 qXb .       dX                     Xb       .        dXp     t
-#dX. 9Xb .dXb    __                         __    dXb.     dXP     .Xb
-#9XXb._ _.dXXXXb dXXXXbo. .odXXXXb dXXXXb._       _.dXXP
-# 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo. .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
-# `9XXXXXXXXXXXXXXXXXXXXX'~ ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
-# `9XXXXXXXXXXXP' `9XX' DIE    `98v8P'  HUMAN   `XXP' `9XXXXXXXXXXXP'
-# ~~~~~~~ 9X.          .db|db.          .XP       ~~~~~~~
-# )b. .dbo.dP'`v'`9b.odb.  .dX(
-# ,dXXXXXXXXXXXb dXXXXXXXXXXXb.
-# dXXXXXXXXXXXP' .   `9XXXXXXXXXXXb
-# dXXXXXXXXXXXXb d|b   dXXXXXXXXXXXXb
-# 9XXb' `XXXXXb.dX|Xb.dXXXXX'   `dXXP
-# `' 9XXXXXX(   )XXXXXXP      `'
-# XXXX X.`v'.X XXXX
-# XP^X'`b d'`X^XX
-# X. 9 `   '  P )X
-# `b `       '  d'
+#        .n                   .                 .                  n.
+#  .   .dP                  dP                   9b                 9b.    .
+# 4    qXb         .       dX                     Xb       .        dXp     t
+#dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+#9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+# 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+#  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+#    `9XXXXXXXXXXXP' `9XX'   DIE    `98v8P'  HUMAN   `XXP' `9XXXXXXXXXXXP'
+#        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+#                        )b.  .dbo.dP'`v'`9b.odb.  .dX(
+#                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+#                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+#                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+#                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   \`dXXP
+#                     \`'      9XXXXXX(   )XXXXXXP      \`'
+#                              XXXX X.\`v'.X XXXX
+#                              XP^X'\`b   d'\`X^XX
+#                              X. 9  \`   '  P )X
+#                              \`b  \`       '  d'
 EOF
 }
 
