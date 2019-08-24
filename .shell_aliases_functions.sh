bf6c3a83ca8b9df2b9dcf3388ca680c73c27a15f
MenkeTechnologies <linux.dev25@gmail.com>
2019-08-23 22:45:09 -0400

    if else

--- .shell_aliases_functions.sh
+++ .shell_aliases_functions.sh
@@ -1662,14 +1662,13 @@ exists http && ge(){
 }
 
 fz(){
-    test -z "$1" && {
-        command ag '^.*$' --color * | fzf -m --delimiter : --nth 3.. --reverse --border --prompt='-->>> ' --preview '[[ -f $(cut -d: -f1 <<< {}) ]] && '"$COLORIZER"' $(cut -d: -f1 <<< {}) '"$COLORIZER_NL"' \
+    if test -z "$1";then
+        command ag '^.*$' --ignore git --color * | fzf -m --delimiter : --nth 3.. --reverse --border --prompt='-->>> ' --preview '[[ -f $(cut -d: -f1 <<< {}) ]] && '"$COLORIZER"' $(cut -d: -f1 <<< {}) '"$COLORIZER_NL"' \
         2>/dev/null | sed -n "$(cut -d: -f2 <<< {}),\$p" || stat $(cut -d: -f1 <<< {}) | fold -80 | head -500' --ansi | cut -d ':' -f1 | perl -pe 's@^(.*)\n$@"$1" @'
-    } || {
-        command ag '^.*$' --color * | fzf --delimiter : --nth 3.. --reverse --border --prompt='-->>> ' --preview '[[ -f $(cut -d: -f1 <<< {}) ]] && '"$COLORIZER"' $(cut -d: -f1 <<< {}) '"$COLORIZER_NL"'\
+    else
+        command ag '^.*$' --ignore git --color * | fzf --delimiter : --nth 3.. --reverse --border --prompt='-->>> ' --preview '[[ -f $(cut -d: -f1 <<< {}) ]] && '"$COLORIZER"' $(cut -d: -f1 <<< {}) '"$COLORIZER_NL"'\
             2>/dev/null | sed -n "$(cut -d: -f2 <<< {}),\$p" || stat $(cut -d: -f1 <<< {}) | fold -80 | head -500' --ansi | perl -pe 's@^(.*?):(\d+):(.*)@+$2 "$1"@'
-
-    }
+    fi
 }
 
 figletfonts(){
