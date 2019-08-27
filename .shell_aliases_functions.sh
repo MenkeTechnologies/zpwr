634d8d28a45a0f37e89165572ca7dedb044f8f55
MenkeTechnologies <linux.dev25@gmail.com>
2019-08-26 20:21:32 -0400

    using gh_reveal zsh plugin

--- .shell_aliases_functions.sh
+++ .shell_aliases_functions.sh
@@ -1125,38 +1125,6 @@ o(){
 
 }
 
-reveal() {
-    open_cmd="$(getOpenCommand)" || return 1
-
-    if [[ -z "$1" ]]; then
-        if ! command git rev-parse --git-dir 2> /dev/null 1>&2;then
-            "$open_cmd" "https://github.com/$GITHUB_ACCOUNT?tab=repositories"
-            return 0
-        fi
-    else
-        if ! command git rev-parse --git-dir 2> /dev/null 1>&2;then
-            for dir in "$@" ; do
-                ( builtin cd "$dir" && reveal; )
-            done
-            return 0
-        fi
-
-    fi
-
-    argValues="$*";
-    command git remote -v | command grep -E "$(echo ${argValues/ /|})" | command grep 'heroku' | command grep fetch | command grep -o -E ':.*' | \
-        cut -c 19- | command awk '{print $1}' | command sed 's@.git$@@' | \
-        command xargs -I {} "$open_cmd" https://dashboard.heroku.com/apps/{} https://{}.herokuapp.com
-
-    {
-        command git remote -v | command grep -E "$(echo ${argValues/ /|})" | command grep '@'  | command grep -o -E '@.*' | cut -c 2-
-        command git remote -v | command grep -E "$(echo ${argValues/ /|})" | command grep '//' | command grep -o -E ':.*' | cut -c 4- | command grep -v 'heroku'
-    } | command grep fetch | command sed 's@:@\/@g' | command awk '{print $1}' | sed 's@.git@@' | command xargs -I {} "$open_cmd" https://www.{}
-    set +x
-
-}
-
-
 openmygh(){
     open_cmd="$(getOpenCommand)" || return 1
 
