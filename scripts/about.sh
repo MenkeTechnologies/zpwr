183413ddc15b9c709a8083996c3d8f33ae8c12b4
MenkeTechnologies <linux.dev25@gmail.com>
2019-08-31 18:30:20 -0400

    about margins

--- scripts/about.sh
+++ scripts/about.sh
@@ -17,8 +17,9 @@ banner(){
 if [[ -d "$ZPWR" ]]; then
     if cd "$ZPWR";then
         version="$(git describe --tags $(git rev-list --tags --max-count=1)| perl -pe 's@[\t ]@@')"
-        fetch="$(git remote -v | head -n 1 | perl -pe 's@[\t ]@    @')"
-        push="$(git remote -v | tail -n 1 | perl -pe 's@[\t ]@    @')"
+        info="$(git tag -n9 "$version" | perl -pe 's@[\t ]+@ @')"
+        fetch="$(git remote -v | head -n 1 | perl -pe 's@[\t ]+@    @')"
+        push="$(git remote -v | tail -n 1 | perl -pe 's@[\t ]+@    @')"
     fi
 fi
 
@@ -39,39 +40,45 @@ printf "\x1b[0m"
 printf "\x1b[35m"
 printf "\x1b[1m"
 cat<<EOF
-                                $version
+                         $info
 EOF
 
 printf "\x1b[0m"
 printf "\x1b[34m"
 cat<<\EOF
 
-                           eeeee  e    e
-                           8   8  8    8
-                           8eee8e 8eeee8
-                           8   8   88
-                           8eee8   88
+                        eeeee  e    e
+                        8   8  8    8
+                        8eee8e 8eeee8
+                        8   8   88
+                        8eee8   88
 EOF
 
 printf "\x1b[0m"
 printf "\x1b[33m"
 echo
 cat<<\EOF
-    ___ ___    ___  ____   __  _    ___  ______    ___     __  __ __  ____
-    |   T   T  /  _]|    \ |  l/ ]  /  _]|      T  /  _]   /  ]|  T  T|    \
-    | _   _ | /  [_ |  _  Y|  ' /  /  [_ |      | /  [_   /  / |  l  ||  _  Y
-    |  \_/  |Y    _]|  |  ||    \ Y    _]l_j  l_jY    _] /  /  |  _  ||  |  |
-    |   |   ||   [_ |  |  ||     Y|   [_   |  |  |   [_ /   \_ |  |  ||  |  |
-    |   |   ||     T|  |  ||  .  ||     T  |  |  |     T\     ||  |  ||  |  |
-    l___j___jl_____jl__j__jl__j\_jl_____j  l__j  l_____j \____jl__j__jl__j__j
-
-    ___   _       ___    ____  ____    ___  _____
-    /   \ | T     /   \  /    Tl    j  /  _]/ ___/
-    Y     Y| |    Y     YY   __j |  T  /  [_(   \_
-    |  O  || l___ |  O  ||  T  | |  | Y    _]\__  T
-    |     ||     T|     ||  l_ | |  | |   [_ /  \ |
-    l     !|     |l     !|     | j  l |     T\    |
-    \___/ l_____j \___/ l___,_j|____jl_____j \___j
+           ___ ___    ___  ____   __  _    ___  
+           |   T   T  /  _]|    \ |  l/ ]  /  _]
+           | _   _ | /  [_ |  _  Y|  ' /  /  [_ 
+           |  \_/  |Y    _]|  |  ||    \ Y    _]
+           |   |   ||   [_ |  |  ||     Y|   [_ 
+           |   |   ||     T|  |  ||  .  ||     T
+           l___j___jl_____jl__j__jl__j\_jl_____j
+             ______    ___     __  __ __  ____
+            |      T  /  _]   /  ]|  T  T|    \
+            |      | /  [_   /  / |  l  ||  _  Y
+            l_j  l_jY    _] /  /  |  _  ||  |  |
+              |  |  |   [_ /   \_ |  |  ||  |  |
+              |  |  |     T\     ||  |  ||  |  |
+              l__j  l_____j \____jl__j__jl__j__j
+         ___   _       ___    ____  ____    ___  _____
+        /   \ | T     /   \  /    Tl    j  /  _]/ ___/
+        Y     Y| |    Y     YY   __j |  T  /  [_(   \_
+        |  O  || l___ |  O  ||  T  | |  | Y    _]\__  T
+        |     ||     T|     ||  l_ | |  | |   [_ /  \ |
+        l     !|     |l     !|     | j  l |     T\    |
+         \___/ l_____j \___/ l___,_j|____jl_____j \___j
 EOF
 echo
 printf "\x1b[0m"
