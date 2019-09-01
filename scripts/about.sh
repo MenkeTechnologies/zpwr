e3185af7040bf96e6919ac775ae2c77fc8c460e6
MenkeTechnologies <linux.dev25@gmail.com>
2019-09-01 00:38:26 -0400

    fix banner chars

--- scripts/about.sh
+++ scripts/about.sh
@@ -18,8 +18,8 @@ if [[ -d "$ZPWR" ]]; then
     if cd "$ZPWR";then
         version="$(git describe --tags $(git rev-list --tags --max-count=1)| perl -pe 's@[\t ]@@')"
         info="$(git tag -l -n9 "$version" | perl -pe 's@[\t ]+@ @')"
-        fetch="$(git remote -v | head -n 1 | perl -pe 's@[\t ]+@    @')"
-        push="$(git remote -v | tail -n 1 | perl -pe 's@[\t ]+@    @')"
+        fetch="$(git remote -v | grep zpwr | grep fetch | head -n 1 | perl -pe 's@[\t ]+@    @')"
+        push="$(git remote -v | grep zpwr | grep push | tail -n 1 | perl -pe 's@[\t ]+@    @')"
     fi
 fi
 
@@ -58,7 +58,7 @@ printf "\x1b[0m"
 printf "\x1b[33m"
 echo
 cat<<\EOF
-           ___ ___    ___  ____   __  _    ___  
+            ___ ___    ___  ____   __  _    ___  
            |   T   T  /  _]|    \ |  l/ ]  /  _]
            | _   _ | /  [_ |  _  Y|  ' /  /  [_ 
            |  \_/  |Y    _]|  |  ||    \ Y    _]
@@ -72,8 +72,8 @@ cat<<\EOF
               |  |  |   [_ /   \_ |  |  ||  |  |
               |  |  |     T\     ||  |  ||  |  |
               l__j  l_____j \____jl__j__jl__j__j
-         ___   _       ___    ____  ____    ___  _____
-        /   \ | T     /   \  /    Tl    j  /  _]/ ___/
+          ___   _       ___    ____  ____    ___  _____
+         /   \ | T     /   \  /    Tl    j  /  _]/ ___/
         Y     Y| |    Y     YY   __j |  T  /  [_(   \_
         |  O  || l___ |  O  ||  T  | |  | Y    _]\__  T
         |     ||     T|     ||  l_ | |  | |   [_ /  \ |
