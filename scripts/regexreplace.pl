0d87c0d1e3d197266c0ebcf7d526b3c7ba141d88
MenkeTechnologies <linux.dev25@gmail.com>
2019-09-05 23:46:45 -0400

    perl tidy in place

--- scripts/regexreplace.pl
+++ scripts/regexreplace.pl
@@ -10,35 +10,39 @@
 use feature 'say';
 
 use Getopt::Long;
-my @files=();
-my $regex = '\r';
+my @files       = ();
+my $regex       = '\r';
 my $replacement = "";
 
-my $help=0;
-GetOptions ('help|?'=>\$help, 'regex=s' => \$regex, 'replacement=s' => \$replacement);
+my $help = 0;
+GetOptions(
+    'help|?'        => \$help,
+    'regex=s'       => \$regex,
+    'replacement=s' => \$replacement
+);
 
-
-if ($help == 1) {
+if ( $help == 1 ) {
     say STDERR "Use --regex or --replacement to specify strings.";
     exit 1;
 }
 
-
 sub addToAry {
-    if (-d $_[0]) {
-        if ($_[0] =~ /\.git/) {
+    if ( -d $_[0] ) {
+        if ( $_[0] =~ /\.git/ ) {
             return;
         }
-        for (glob("$_[0]/* $_[0]/.[!.]*")){
+        for ( glob("$_[0]/* $_[0]/.[!.]*") ) {
             addToAry($_);
         }
-    } else {
-    say "<<<($_[0])>>>";
+    }
+    else {
+        say "<<<($_[0])>>>";
         my $type = `file $_[0]`;
-        if ($type =~ /text/) {
+        if ( $type =~ /text/ ) {
             my $out = `cat $_[0]`;
-            if ($out =~ /$regex/){
-                say "\x1b[34;1m<<<(\x1b[0;34m$_[0]\x1b[0m has got \x1b[34m$regex!\x1b[0;34;1m)>>>\x1b[0m";
+            if ( $out =~ /$regex/ ) {
+                say
+"\x1b[34;1m<<<(\x1b[0;34m$_[0]\x1b[0m has got \x1b[34m$regex!\x1b[0;34;1m)>>>\x1b[0m";
                 push @files, $_[0];
             }
         }
@@ -48,26 +52,29 @@ sub addToAry {
 addToAry $_ for @ARGV;
 my $length = scalar @files;
 
-if ($length > 0){
+if ( $length > 0 ) {
+
     #open $less, "|-","less -MN" or die $!;
     #select $less;
     say "";
-    say "*"x80 for (0..2);
+    say "*" x 80 for ( 0 .. 2 );
     say "";
-    say "\x1b[34;1m<<<(\x1b[0;34m$_\x1b[0m has got \x1b[34m$regex!\x1b[0;34;1m)>>>\x1b[0m" for @files;
+    say
+"\x1b[34;1m<<<(\x1b[0;34m$_\x1b[0m has got \x1b[34m$regex!\x1b[0;34;1m)>>>\x1b[0m"
+      for @files;
 
-    say "\x1b[34;1m<<<(\x1b[0;34m$length \x1b[0;34mfiles!\x1b[0;34;1m)>>>\x1b[0m";
+    say
+      "\x1b[34;1m<<<(\x1b[0;34m$length \x1b[0;34mfiles!\x1b[0;34;1m)>>>\x1b[0m";
     print "Remove $regex?\x1b[1;34m>\x1b[0m ";
     my $answer = <STDIN>;
     chomp $answer;
-    if ($answer =~ /y|yes/i) {
+    if ( $answer =~ /y|yes/i ) {
         `perl -i -pe 's#$regex#$replacement#g' "$_"` for @files;
-    } else {
+    }
+    else {
         say "bye";
     }
+
     #close $less;
 }
-
-
-
 
