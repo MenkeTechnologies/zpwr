b0705e5812f02693f37c1d076f44aac48ac9f5eb
MenkeTechnologies <linux.dev25@gmail.com>
2019-09-06 19:02:42 -0400

    fzf maps and shell scripts for all vim keybindings

--- scripts/keybindingsToFZF.zsh
+++ scripts/keybindingsToFZF.zsh
@@ -8,52 +8,27 @@
 #}}}***********************************************************
 
 {
-    echo "# Tmux keybindings (tmux lsk)"
     tmux lsk
-
-    echo "# Zsh Vim Insert Mode keybindings (bindkey -M viins -L)"
     bindkey -M viins -L
-
-    echo "# Zsh Vim Normal Mode keybindings (bindkey -M vicmd -L)"
     bindkey -M vicmd -L
-
-    echo "# Zsh Menuselect Mode keybindings (bindkey -M menuselect -L)"
     bindkey -M menuselect -L
-
-    echo "# Zsh Vim Visual Mode keybindings (bindkey -M visual -L)"
     bindkey -M visual -L
-
-    echo "# Zsh Listscroll Mode keybindings (bindkey -M listscroll -L)"
     bindkey -M listscroll -L
-
-    echo "# Zsh Vim Operator Mode keybindings (bindkey -M viopp -L)"
     bindkey -M viopp -L
-
-    printf "# Vim Keybindings Insert Mode (:imap)"
     vim -e -c 'redir > ~/.temp1 | silent imap | redir END | quitall'
     cat ~/.temp1
-    echo
-
-    printf "# Vim Keybindings Normal Mode (:nmap)"
     vim -e -c 'redir > ~/.temp2 | silent nmap | redir END | quitall'
     cat ~/.temp2
-    echo
-
-    printf "# Vim Keybindings Visual Mode (:vmap)"
     vim -e -c 'redir > ~/.temp3 | silent vmap | redir END | quitall'
     cat ~/.temp3
-    echo
-
-    printf "# Vim Keybindings Command Colon Mode (:cmap)"
     vim -e -c 'redir > ~/.temp4 | silent cmap | redir END | quitall'
     cat ~/.temp4
-    echo
-
     command rm ~/.temp{1..4}
 
 } | perl -ne 'print if /\S+/' > ~/.temp$$
 
 #do not know why have to create tempfile here
-perl -pe 's@^([^#].*)$@- ```$1```@g' ~/.temp$$ | perl -pe 's@(.*) \(:.map\).*@$1@'
+perl -pe 's@^([^#].*)$@$1@g' ~/.temp$$ | perl -pe 's@(.*) \(:.map\).*@$1@'
 
 command rm ~/.temp$$
+
