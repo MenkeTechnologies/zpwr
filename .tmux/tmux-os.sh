a0d2bf0d0e7da58c1e0390e26823b4aa18f1f3aa
MenkeTechnologies <linux.dev25@gmail.com>
2019-09-07 02:30:35 -0400

    wrap in fxn

--- .tmux/tmux-os.sh
+++ .tmux/tmux-os.sh
@@ -14,29 +14,32 @@ logg(){
 exec 2>> ~/updaterlog.txt
 exec 1>> ~/updaterlog.txt
 set -x
-if uname | grep -q Darwin; then
-    tmux source-file "$HOME/.tmux/tmux-mac"
-    exit 0
-elif uname | grep -q Linux; then
-    pyv=$(python3 --version)
-    logg "python version: $pyv"
-    if echo "$pyv" | grep -q 3.4; then
-        tmux source-file "$HOME/.tmux/tmux-linux-34"
-        logg "done 34"
+verify_tmux_version () {
+    if uname | grep -q Darwin; then
+        tmux source-file "$HOME/.tmux/tmux-mac"
         exit 0
-    elif echo "$pyv" | grep -q 3.5; then
-        tmux source-file "$HOME/.tmux/tmux-linux-35"
-        logg "done 35"
-        exit 0
-    elif echo "$pyv" | grep -q 3.6; then
-        tmux source-file "$HOME/.tmux/tmux-linux-36"
-        logg "done 36"
-        exit 0
-    elif echo "$pyv" | grep -q 3.7; then
-        tmux source-file "$HOME/.tmux/tmux-linux-37"
-        logg "done 37"
+    elif uname | grep -q Linux; then
+        pyv=$(python3 --version)
+        logg "python version: $pyv"
+        if echo "$pyv" | grep -q 3.4; then
+            tmux source-file "$HOME/.tmux/tmux-linux-34"
+            logg "done 34"
+            exit 0
+        elif echo "$pyv" | grep -q 3.5; then
+            tmux source-file "$HOME/.tmux/tmux-linux-35"
+            logg "done 35"
+            exit 0
+        elif echo "$pyv" | grep -q 3.6; then
+            tmux source-file "$HOME/.tmux/tmux-linux-36"
+            logg "done 36"
+            exit 0
+        elif echo "$pyv" | grep -q 3.7; then
+            tmux source-file "$HOME/.tmux/tmux-linux-37"
+            logg "done 37"
+            exit 0
+        fi
         exit 0
     fi
-    exit 0
-fi
+}
 
+verify_tmux_version
