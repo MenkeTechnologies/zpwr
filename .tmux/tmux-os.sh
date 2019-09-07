f8ce4e18cdcb77c7c6dcda47710795bf2a010905
MenkeTechnologies <linux.dev25@gmail.com>
2019-09-07 02:12:01 -0400

    exit 0

--- .tmux/tmux-os.sh
+++ .tmux/tmux-os.sh
@@ -14,21 +14,26 @@ logg(){
 set -x
 if uname | grep -q Darwin; then
     tmux source-file "$HOME/.tmux/tmux-mac"
+    exit 0
 elif uname | grep -q Linux; then
     pyv=$(python3 --version)
     logg "python version: $pyv"
     if echo "$pyv" | grep -q 3.4; then
         tmux source-file "$HOME/.tmux/tmux-linux-34"
         logg "done 34"
+        exit 0
     elif echo "$pyv" | grep -q 3.5; then
         tmux source-file "$HOME/.tmux/tmux-linux-35"
         logg "done 35"
+        exit 0
     elif echo "$pyv" | grep -q 3.6; then
         tmux source-file "$HOME/.tmux/tmux-linux-36"
         logg "done 36"
+        exit 0
     elif echo "$pyv" | grep -q 3.7; then
         tmux source-file "$HOME/.tmux/tmux-linux-37"
         logg "done 37"
+        exit 0
     fi
 fi
 
