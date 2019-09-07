93c8fb1f79f43a052f4bd22b7f4a8dda8d6d7213
MenkeTechnologies <linux.dev25@gmail.com>
2019-09-07 03:10:49 -0400

    forgot to update variable in msg

--- .shell_aliases_functions.sh
+++ .shell_aliases_functions.sh
@@ -2014,7 +2014,7 @@ needSudo(){
 
 updatePowerlineLink(){
     dir="$(sudo python3 -m pip show powerline-status | \grep --color=always '^Location' | awk '{print $2}')/powerline"
-    prettyPrint "linking $dir to $HOME/powerline"
+    prettyPrint "linking $dir to $TMUX_HOME/powerline"
     ln -s "$dir" "$TMUX_HOME/powerline"
 }
 
