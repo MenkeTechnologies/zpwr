4d9ecf70d134cba48dedbb1b3bbee2bad1e6f0f9
MenkeTechnologies <linux.dev25@gmail.com>
2019-09-20 11:11:01 -0400

    man 2> dev null

--- .shell_aliases_functions.sh
+++ .shell_aliases_functions.sh
@@ -719,7 +719,7 @@ clearList() {
                         prettyPrint "STATS:"
                         stat "$lf"
                         prettyPrint "MAN:"
-                        man -wa "$(basename $lf)"
+                        man -wa "$(basename $lf)" 2>/dev/null
                         echo
                         echo
                     else
