33785fcc276c21a7077f71e7cd0637fae5205b10
MenkeTechnologies <linux.dev25@gmail.com>
2019-09-22 14:53:02 -0400

    go install script

---
+++ go_install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#{{{ MARK:Header
+#**************************************************************
+##### Author: MenkeTechnologies
+##### GitHub: https://github.com/MenkeTechnologies
+##### Date: Sun Sep 22 14:50:21 EDT 2019
+##### Purpose: bash script to
+##### Notes:
+#}}}***********************************************************
+
+export GOPATH="$HOME/go"
+prettyPrint "Installing mylg"
+go get github.com/mehrdadrad/mylg
+cd "$HOME/go/src/github.com/mehrdadrad/mylg/"
+go build mylg.go
+
+prettyPrint "Installing gotop"
+go get github.com/cjbassi/gotop
+
+prettyPrint "Installing lolcat in go"
+go get github.com/MenkeTechnologies/lolcat
