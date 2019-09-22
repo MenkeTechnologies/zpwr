a6ee71f85c7017b03568c4beaaa786aa522c74b8
MenkeTechnologies <linux.dev25@gmail.com>
2019-09-22 14:49:57 -0400

    using lolcat in go

--- .shell_aliases_functions.sh
+++ .shell_aliases_functions.sh
@@ -2069,6 +2069,31 @@ regenPowerlineLink(){
     command tmux source-file "$HOME/.tmux.conf"
 }
 
+goclean() {
+ local pkg=$1;
+ shift || return 1
+ local ost
+ local cnt
+ local scr
+
+ # Clean removes object files from package source directories (ignore error)
+ go clean -i $pkg &>/dev/null
+
+ # Set local variables
+ [[ "$(uname -m)" == "x86_64" ]] && ost="$(uname)"
+ ost="${ost,,}_amd64" && cnt="${pkg//[^\/]}"
+
+ # Delete the source directory and compiled package directory(ies)
+ if (("${#cnt}" == "2")); then
+  rm -rf "${GOPATH%%:*}/src/${pkg%/*}"
+  rm -rf "${GOPATH%%:*}/pkg/${ost}/${pkg%/*}"
+ elif (("${#cnt}" > "2")); then
+  rm -rf "${GOPATH%%:*}/src/${pkg%/*/*}"
+  rm -rf "${GOPATH%%:*}/pkg/${ost}/${pkg%/*/*}"
+ fi
+
+}
+
 
 #}}}***********************************************************
 
