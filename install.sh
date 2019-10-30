fdc502e87dab44fff4389bda7e1da29c08e8e7cb
MenkeTechnologies <linux.dev25@gmail.com>
2019-10-30 11:29:58 -0400

    no python2

--- install.sh
+++ install.sh
@@ -531,8 +531,7 @@ if echo "$vimV >= 8.0" | bc | grep -q 1 || vim --version 2>&1 | grep -q '\-pytho
         ./configure --with-features=huge \
             --enable-multibyte \
             --enable-rubyinterp=yes \
-            --enable-pythoninterp=yes \
-            --with-python-config-dir=/usr/lib/python2.7/config \
+            --enable-pythoninterp=no \
             --enable-python3interp=yes \
             --with-python3-config-dir=/usr/lib/python3.5/config \
             --enable-perlinterp=yes \
