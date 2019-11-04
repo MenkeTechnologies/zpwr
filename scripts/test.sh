ac107e6633761524c5bb2f0a819809b1dc287aa9
MenkeTechnologies <linux.dev25@gmail.com>
2019-11-03 22:35:06 -0500

    local clipboard_str

--- scripts/test.sh
+++ scripts/test.sh
@@ -26,8 +26,10 @@ if [[ -z "$WIZARD" ]]; then
     ##################################
 
     echo "we are 2019-01-11 Jan 11, 2019 2019-01-11 13:33"
+    echo "the date is 2019-11-03 02:09"
 
 fi
+
 echo cool
 echo cool
 echo cool
@@ -45,6 +47,8 @@ else
     ####
     echo "the dtae is 2019-01-11"
 fi
+
+echo i am cool ok
 
 if ((x > 5000)); then
     echo "we are here inside the house and why not 2019-01-15 was Jan 15, 2019 lol Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
