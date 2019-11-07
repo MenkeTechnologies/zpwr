42ef76b2a2cb2b7dc30b8ccd1dde4d83ac0c7c95
MenkeTechnologies <linux.dev25@gmail.com>
2019-11-07 00:32:14 -0500

    create new updater for pis

---
+++ scripts/updatePi.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+#{{{ MARK:Header
+#**************************************************************
+##### Author: JACOBMENKE
+##### Date: Mon Jul 10 12:03:45 EDT 2017
+##### Purpose: bash script to update all PIs
+##### Notes:
+#}}}***********************************************************
+
+updatePI() {
+    #first argument is user@host and port number configured in .ssh/config
+    #-t to force pseudoterminal allocation for interactive programs on remote host
+    #pipe yes into programs that require confirmation
+    #alternatively apt-get has -y option
+    # -x option to disable x11 forwarding
+
+    hostname="$(echo "$1" | awk -F: '{print $1}')"
+    manager="$(echo "$1" | awk -F: '{print $2}')"
+    prettyPrint "Updating $hostname with $manager"
+
+    if [[ "$manager" == "apt" ]]; then
+        ssh -x "$hostname" '
+        yes | sudo apt update
+        yes | sudo apt dist-upgrade
+        yes | sudo apt autoremove
+        yes | sudo apt autoclean'
+    elif [[ "$manager" == zypper ]]; then
+        ssh -x "$hostname" 'sudo zypper --non-interactive refresh
+        sudo zypper --non-interactive update
+        sudo zypper --non-interactive dist-upgrade
+        sudo zypper --non-interactive clean -a'
+    elif [[ "$manager" == dnf ]]; then
+        ssh -x "$hostname" 'yes | sudo dnf upgrade
+        yes | sudo dnf clean all'
+    else
+        :
+    fi
+
+    #update python packages
+    ssh -x "$hostname" bash <"$SCRIPTS/pipUpdater.sh"
+    #here we will update the Pi's own software and vim plugins (not included in apt-get)
+    #avoid sending commmands from stdin into ssh, better to send stdin script into bash
+    ssh -x "$hostname" bash <"$SCRIPTS/rpiSoftwareUpdater.sh"
+}
+
+#for loop through arrayOfPI, each item in array is item is .ssh/config file for
+for pi in "${PI_ARRAY[@]}"; do
+    updatePI "$pi"
+done
