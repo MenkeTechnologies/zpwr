38daffe1c43f5291a86d434ff025a4c01f8284c6
MenkeTechnologies <linux.dev25@gmail.com>
2019-11-09 12:18:24 -0500

    use neovim for generating keybindings

--- scripts/keybindingsToFZFVim.zsh
+++ scripts/keybindingsToFZFVim.zsh
@@ -6,18 +6,23 @@
 ##### Purpose: zsh script to gen the keybindings for README
 ##### Notes:
 #}}}***********************************************************
+if [[ $USE_NEOVIM == true ]]; then
+    cmd=nvim
+else
+    cmd=vim
+fi
 
 if [[ -z "$TEMPFILE" ]]; then
     TEMPFILE="/tmp/.temp$$"
 fi
 {
-    vim -e -c 'redir! > ~/.temp1 | silent imap | redir END | quitall'
+    $cmd -e -c 'redir! > ~/.temp1 | silent imap | redir END | quitall'
     cat ~/.temp1
-    vim -e -c 'redir! > ~/.temp2 | silent nmap | redir END | quitall'
+    $cmd -e -c 'redir! > ~/.temp2 | silent nmap | redir END | quitall'
     cat ~/.temp2
-    vim -e -c 'redir! > ~/.temp3 | silent vmap | redir END | quitall'
+    $cmd -e -c 'redir! > ~/.temp3 | silent vmap | redir END | quitall'
     cat ~/.temp3
-    vim -e -c 'redir! > ~/.temp4 | silent cmap | redir END | quitall'
+    $cmd -e -c 'redir! > ~/.temp4 | silent cmap | redir END | quitall'
     cat ~/.temp4
     command rm ~/.temp{1..4}
 
