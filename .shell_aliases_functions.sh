ee5c6408e5d95b942f3b0472d76cec45802e243c
MenkeTechnologies <linux.dev25@gmail.com>
2019-11-11 22:44:22 -0500

    extended env var

--- .shell_aliases_functions.sh
+++ .shell_aliases_functions.sh
@@ -102,7 +102,12 @@ export LOGFILE="$HOME/updaterlog.txt"
 export UMASK=077
 export LESS="-M -N -R -K -F -X"
 export PSQL_EDITOR='vim -c "setf sql"'
-export EXA_COMMAND='command exa --git -il -F -H --extended --color-scale -g -a --colour=always'
+if [[ $EXA_EXTENDED == true ]]; then
+    export EXA_COMMAND='command exa --git -il -F -H --extended --color-scale -g -a --colour=always'
+else
+    export EXA_COMMAND='command exa --git -il -F -H --color-scale -g -a --colour=always'
+fi
+
 export USE_NEOVIM=true
 #}}}***********************************************************
 
