73f1818eb3c66ada99c183e1903c80e7421ba99b
MenkeTechnologies <linux.dev25@gmail.com>
2019-11-22 12:12:50 -0500

    start containers script

---
+++ scripts/startContainers.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#{{{ MARK:Header
+#**************************************************************
+##### Author: MenkeTechnologies
+##### GitHub: https://github.com/MenkeTechnologies
+##### Date: Fri Nov 22 12:09:38 EST 2019
+##### Purpose: bash script to
+##### Notes:
+#}}}***********************************************************
+
+sudo docker-compose -f $HOME/docker/zabbix-docker/docker-compose_v3_ubuntu_pgsql_latest.yaml up
+sudo docker-compose -f $HOME/docker/docker-elk/docker-compose.yml up
