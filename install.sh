33a7b17c04ea5fc42964b97f6cf4d47540cbab38
MenkeTechnologies <linux.dev25@gmail.com>
2019-12-23 15:10:12 -0500

    more ZPWR_SCRIPTS changes

--- install.sh
+++ install.sh
@@ -705,7 +705,7 @@ prettyPrint "Done!!!!!!"
 prettyPrint "Starting Tmux..."
 prettyPrint "Starting the matrix"
 export SHELL="$(which zsh)"
-export SCRIPTS="$HOME/.zpwr/scripts"
+export ZPWR_SCRIPTS="$HOME/.zpwr/scripts"
 dir="$(sudo python3 -m pip show powerline-status | \grep --color=always '^Location' | awk '{print $2}')/powerline"
 prettyPrint "linking $dir to ~/.tmux/powerline"
 
