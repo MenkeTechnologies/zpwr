12fdfddd7573ed5888dba18b17d862db02ca7d01
MenkeTechnologies <linux.dev25@gmail.com>
2020-01-02 10:01:32 -0800

    install Npm function

--- npm_install.sh
+++ npm_install.sh
@@ -9,48 +9,52 @@
 #}}}***********************************************************
 
 source common.sh || { echo "Must be in zpwr directory" >&2; exit 1; }
+function installNpm(){
+    prettyPrint "curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -"
+    curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+    prettyPrint "install nodesj"
+    update "nodejs" "$distroFamily"
+    prettyPrint "install buid-essential"
+    update "build-essential" "$distroFamily"
+}
 
 if [[ "$ZPWR_OS_TYPE" == "darwin" ]]; then
-        distroName=Mac
-        distroFamily=mac
-    elif [[ "$ZPWR_OS_TYPE" == linux ]];then
-        distroName=$(perl -lne 'do{($_=$1)=~s/"//g;print;exit0}if/^ID=(.*)/' /etc/os-release)
-        case $distroName in
-            (debian|ubuntu|elementary|raspbian|kali|linuxmint|zorin|parrot)
-                distroFamily=debian
-                ;;
-            (arch)
-                distroFamily=arch
-                ;;
-            (*suse*)
-                distroFamily=suse
-                ;;
-            (centos|fedora|rhel)
-                distroFamily=redhat
-                ;;
-            (*)
-                prettyPrint "Your distroFamily $distroName is unsupported!" >&2
-                exit 1
-                ;;
+    distroName=Mac
+    distroFamily=mac
+elif [[ "$ZPWR_OS_TYPE" == linux ]];then
+    distroName=$(perl -lne 'do{($_=$1)=~s/"//g;print;exit0}if/^ID=(.*)/' /etc/os-release)
+    case $distroName in
+        (debian|ubuntu|elementary|raspbian|kali|linuxmint|zorin|parrot)
+            distroFamily=debian
+            ;;
+        (arch)
+            distroFamily=arch
+            ;;
+        (*suse*)
+            distroFamily=suse
+            ;;
+        (centos|fedora|rhel)
+            distroFamily=redhat
+            ;;
+        (*)
+            prettyPrint "Your distroFamily $distroName is unsupported!" >&2
+            exit 1
+            ;;
     esac
-
-    else
+    installNpm
+else
     if [[ "$ZPWR_OS_TYPE" == freebsd ]]; then
         distroFamily=freebsd
         distroName=FreeBSD
+        installNpm
+    else
+        prettyPrint "Your distroFamily $distroName is unsupported!" >&2
     fi
-
 fi
 
 
-
-
-curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
-
-update "nodejs" "$distroFamily"
-update "build-essential" "$distroFamily"
-
 exists diff-so-fancy || {
+    prettyPrint "npm installing diff-so-fancy"
     sudo npm i -g diff-so-fancy
 }
 prettyPrint "installing neovim nodejs lib"
