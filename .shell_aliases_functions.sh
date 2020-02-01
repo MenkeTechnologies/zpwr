39e7be47f9da1a01fa761cede01227353fdc69ca
MenkeTechnologies <linux.dev25@gmail.com>
2020-02-01 14:18:32 -0500

    fix brc alias

--- .shell_aliases_functions.sh
+++ .shell_aliases_functions.sh
@@ -297,7 +297,7 @@ isZsh && {
 #{{{                    MARK:ALIASES for editing config files
 #**************************************************************
 alias vrc="vim -S ~/.vim/sessions/vrc.vim ~/.vimrc"
-alias brc="vim -S ~/.vim/sessions/aliases.vim + $ZPWR_LOCAL/.shell_aliases_functions.sh; source $ZPWR_LOCAL/.shell_aliases_functions.sh; bash $ZPWR_SCRIPTS/backupBashConfig.sh 2> /dev/null"
+alias brc="vim -S ~/.vim/sessions/aliases.vim + $ZPWR/.shell_aliases_functions.sh; source $ZPWR/.shell_aliases_functions.sh; bash $ZPWR_SCRIPTS/backupBashConfig.sh 2> /dev/null"
 alias zrc="vim -S ~/.vim/sessions/zshrc.vim + ~/.zshrc; source ~/.zshrc"
 alias trc="vim -S ~/.vim/sessions/trc.vim ~/.tmux.conf"
 #}}}***********************************************************
