68f70d0d89aa34bbb835ddec19c7fe9b6806f48c
MenkeTechnologies <linux.dev25@gmail.com>
2020-02-19 01:55:34 -0500

    missing manjaro deps

--- install/install.sh
+++ install/install.sh
@@ -203,7 +203,7 @@ addDependenciesLinux(){
         samba samba-common scrot syslog-ng sshfs fuse tomcat8 golang xclip strace)
     }
 addDependenciesArch(){
-    dependencies_ary+=(go linux-headers net-tools)
+    dependencies_ary+=(autoconf make the_silver_searcher go linux-headers net-tools)
 }
 
 addDependenciesSuse(){
