ffc04d465c1984f79227012b119e592e363e38ee
MenkeTechnologies <linux.dev25@gmail.com>
2020-04-24 20:51:59 -0400

    add back missing daemon

--- scripts/em-server.sh
+++ scripts/em-server.sh
@@ -13,11 +13,11 @@ if echo "$out" | command grep -i -qs 'emacs.*bg-daemon'; then
 else
     loggConsolePrefix "starting emacs daemon"
     echo "$out" | command grep -s 'emacs.*bg-daemon'
-    command emacs -nw 2>> "$ZPWR_LOGFILE" 1>&2
+    command emacs -nw --daemon 2>> "$ZPWR_LOGFILE" 1>&2
 fi
 
 # if there is a frame
-if emacsclient -n -s instance1 -e "(if (> (length (frame-list)) 1) 't)" 2> /dev/null | command grep -sq t; then
+if emacsclient -n -e "(if (> (length (frame-list)) 1) 't)" 2> /dev/null | command grep -sq t; then
     #attach client
     loggConsolePrefix "attach frame"
     command emacsclient -nw "$@"
