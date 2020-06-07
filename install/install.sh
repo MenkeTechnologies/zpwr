09120961ea4a59f856793e4bed2dce98d4310fce
MenkeTechnologies <linux.dev25@gmail.com>
2020-06-07 00:38:13 -0400

    move to new homebrew installer

--- install/install.sh
+++ install/install.sh
@@ -394,7 +394,7 @@ if [[ "$ZPWR_OS_TYPE" == "darwin" ]]; then
         if exists "brew"; then
             # install homebrew
             prettyPrintBox "Installing HomeBrew..."
-            /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
         fi
 
         if ! exists "brew"; then
