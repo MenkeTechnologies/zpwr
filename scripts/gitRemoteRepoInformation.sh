9a6e99b5b5fef8fc3e991d375d086b6c2e518f94
MenkeTechnologies <linux.dev25@gmail.com>
2020-09-20 03:07:24 -0400

    perl regex in git remote info script

--- scripts/gitRemoteRepoInformation.sh
+++ scripts/gitRemoteRepoInformation.sh
@@ -7,23 +7,54 @@
 ##### Notes:
 #}}}***********************************************************
 
+if [[ -n "$ZPWR" && -n "$ZPWR_LIB_INIT" ]]; then
+    if ! source "$ZPWR_LIB_INIT" ""; then
+        echo "Could not source dir '$ZPWR_LIB_INIT'."
+        exit 1
+    fi
+else
+    source="${BASH_SOURCE[0]}"
+    while [ -h "$source" ]; do # resolve $source until the file is no longer a symlink
+    zpwrBaseDir="$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )"
+    source="$(readlink "$source")"
+    [[ $source != /* ]] && source="$zpwrBaseDir/$source" # if $source was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    zpwrBaseDir="$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )"
+
+    while [[ ! -f "$zpwrBaseDir/.zpwr_root" ]]; do
+        zpwrBaseDir="$(dirname "$zpwrBaseDir")"
+        if [[ "$zpwrBaseDir" == / ]]; then
+            echo "Could not find .zpwr_root file up the directory tree." >&2
+            exit 1
+        fi
+    done
+    if ! source "$zpwrBaseDir/scripts/init.sh" "$zpwrBaseDir"; then
+        echo "Could not source zpwrBaseDir '$zpwrBaseDir/scripts/init.sh'."
+        exit 1
+    fi
+
+    unset zpwrBaseDir
+fi
+
 for directory; do
     if [[ -d "$directory" ]]; then
         cd "$directory" && {
-            git rev-parse --git-dir &>/dev/null && {
-                line="$(git remote -v 2>/dev/null | sed 1q)" && {
+            isGitDir && {
+                line="$(git remote -v 2>/dev/null)" && {
                     if echo "$line" | grep -q 'git@'; then
                         #ssh
-                        user="$(echo $line | awk -F':' '{print $2}' | awk -F'/' '{print $1}')"
-                        repo="$(echo $line | awk -F'/' '{print $2}' | awk '{print $1}')"
+                        out="$(echo $line | perl -ne 'do{print "$1";exit 0} if m{^.*:(\S+/\S+)\.git\s+.*$}')"
+                        #user="${out%/*}"
+                        #repo="${out#*/}"
                     else
                         #http
-                        user="$(echo $line | awk -F'/' '{print $4}')"
-                        repo="$(echo $line | awk -F'/' '{print $5}' | awk '{print $1}')"
+                        out="$(echo $line | perl -ne 'do{print "$1";exit 0} if m{^.*/(\S+/\S+)\s+.*$}')"
                     fi
-                    echo "$user/${repo%%.git*}"
+                    echo "$out"
                 }
             }
         }
     fi
 done
+
+unset directory
