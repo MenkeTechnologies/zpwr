ded43efd96f7b10c4a19d940a2d59a389dd3284d
MenkeTechnologies <linux.dev25@gmail.com>
2021-06-29 21:58:54 -0400

    add missing distribution cases

--- install/pip_install.sh
+++ install/pip_install.sh
@@ -104,16 +104,16 @@ elif [[ "$ZPWR_OS_TYPE" == "linux" ]];then
         distroName=$(perl -lne 'do{($_=$1)=~s/"//g;print;exit0}if/^ID=(.*)/' /etc/os-release)
 
         case $distroName in
-            (debian|ubuntu|elementary|raspbian|kali|linuxmint|zorin|parrot)
+            (debian | ubuntu* | elementary* | raspbian | kali | linuxmint | zorin | parrot)
                 distroFamily=debian
                 ;;
-            (arch|endeavouros | manjaro*)
+            (arch | endeavouros | manjaro*)
                 distroFamily=arch
                 ;;
             (*suse*)
                 distroFamily=suse
                 ;;
-            (centos|fedora|rhel|amzn)
+            (centos | fedora | rhel | amzn)
                 distroFamily=redhat
                 ;;
             (*)
--- scripts/colorLogger.sh
+++ scripts/colorLogger.sh
@@ -69,15 +69,15 @@ else
     distroName=$(perl -lne 'do{($_=$1)=~s/"//g;print;exit0}if/^ID=(.*)/' /etc/os-release)
 
     case "$distroName" in
-    debian | ubuntu | elementary | raspbian | kali | zorin | parrot)
-        distro=debian
-        ;;
-    centos | fedora | rhel | amzn)
-        distro=redhat
-        ;;
-    opensuse* | suse* | arch | endeavouros | manjaro*)
-        distro=suse
-        ;;
+        (debian | ubuntu* | elementary* | raspbian | kali | linuxmint | zorin | parrot)
+            distro=debian
+            ;;
+        (centos | fedora | rhel | amzn)
+            distro=redhat
+            ;;
+        (opensuse* | suse* | arch | endeavouros | manjaro*)
+            distro=suse
+            ;;
     esac
 
     if [[ "$distro" == debian ]]; then
