48d6ef1f43dbdd45b67e2ae14d149247eaf4bc6d
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-15 03:04:59 -0500

    move homebrew environment variables outside of path blocker

--- env/.shell_aliases_functions.sh
+++ env/.shell_aliases_functions.sh
@@ -68,6 +68,27 @@ if [[ -z "$TMUX" ]]; then
 else
     export NCURSES_NO_UTF8_ACS=1
 fi
+
+if [[ "$ZPWR_OS_TYPE" == darwin ]]; then
+    #export CPATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include"
+
+    if [[ -d "/opt/homebrew" ]]; then
+        export HOMEBREW_PREFIX='/opt/homebrew'
+        export HOMEBREW_HOME_FORMULAE="$HOMEBREW_PREFIX/Library/taps/homebrew/homebrew-core/formula"
+        export NODE_HOME="$HOMEBREW_PREFIX/lib/node_modules"
+        export NODE_PATH="$NODE_HOME:$YARN_HOME/global/node_modules"
+    else
+        export HOMEBREW_PREFIX='/usr/local'
+        export HOMEBREW_HOME_FORMULAE="$HOMEBREW_PREFIX/Homebrew/Library/taps/homebrew/homebrew-core/formula"
+    fi
+    export HOMEBREW_CELLAR="$HOMEBREW_PREFIX/Cellar"
+    export HOMEBREW_OPT_HOME="$HOMEBREW_PREFIX/opt"
+    export HOMEBREW_DBHOME="$HOMEBREW_PREFIX/var"
+    export HOMEBREW_DB_CONF="$HOMEBREW_PREFIX/etc"
+    export PIP3_HOME="$HOMEBREW_PREFIX/lib/$ZPWR_PYTHON/site-packages"
+else
+    export PIP3_HOME="/usr/local/lib/$ZPWR_PYTHON/site-packages"
+fi
 #}}}***********************************************************
 
 #{{{                    MARK:PATH
@@ -84,21 +105,8 @@ if ! echo "$PATH" | command grep -isq "$ZPWR_SCRIPTS"; then
 
         if [[ -d "/opt/homebrew" ]]; then
             export PATH="/opt/homebrew/bin:$PATH"
-            export HOMEBREW_PREFIX='/opt/homebrew'
-            export HOMEBREW_HOME_FORMULAE="$HOMEBREW_PREFIX/Library/taps/homebrew/homebrew-core/formula"
-            export NODE_HOME="$HOMEBREW_PREFIX/lib/node_modules"
-            export NODE_PATH="$NODE_HOME:$YARN_HOME/global/node_modules"
-        else
-            export HOMEBREW_PREFIX='/usr/local'
-            export HOMEBREW_HOME_FORMULAE="$HOMEBREW_PREFIX/Homebrew/Library/taps/homebrew/homebrew-core/formula"
         fi
-        export HOMEBREW_CELLAR="$HOMEBREW_PREFIX/Cellar"
-        export HOMEBREW_OPT_HOME="$HOMEBREW_PREFIX/opt"
-        export HOMEBREW_DBHOME="$HOMEBREW_PREFIX/var"
-        export HOMEBREW_DB_CONF="$HOMEBREW_PREFIX/etc"
-        export PIP3_HOME="$HOMEBREW_PREFIX/lib/$ZPWR_PYTHON/site-packages"
     else
-        export PIP3_HOME="/usr/local/lib/$ZPWR_PYTHON/site-packages"
         export PATH="$PATH:/usr/games"
     fi
 
--- scripts/macOnly/figletRandomFontOnce.sh
+++ scripts/macOnly/figletRandomFontOnce.sh
@@ -8,7 +8,7 @@
 #}}}***********************************************************
 
 [[ "$(uname)" == Darwin ]] && {
-    FIGLET_DIR="$HOMEBREW_CELLAR/figlet/2.2.5/share/figlet/fonts"
+    FIGLET_DIR="$HOMEBREW_OPT_HOME/figlet/share/figlet/fonts"
 } || {
     FIGLET_DIR="/usr/share/figlet"
 }
@@ -23,16 +23,19 @@ done < <(find "$FIGLET_DIR" -iname "*.flf" 2>/dev/null)
 
 rangePossibleIndices=${#ary[*]}
 
-randIndex=$(($RANDOM % $rangePossibleIndices))
-font=${ary[$randIndex]}
+if (( $rangePossibleIndices > 0 )); then
+    randIndex=$(($RANDOM % $rangePossibleIndices))
+    font=${ary[$randIndex]}
 
-echo "$(date) random font is $font" >>"$ZPWR_LOGFILE"
+    echo "$(date) random font is $font" >>"$ZPWR_LOGFILE"
 
-if (($# == 0)); then
-    cat | figlet -f "$font"
-else
-    output="$(echo $TEXT_TO_DISPLAY | figlet -f $font)"
+    if (($# == 0)); then
+        cat | figlet -f "$font"
+    else
+        output="$(echo $TEXT_TO_DISPLAY | figlet -f $font)"
 
-    [[ -n "$FILTER" ]] && echo "$output" | "$FILTER" || echo "$output"
+        [[ -n "$FILTER" ]] && echo "$output" | "$FILTER" || echo "$output"
 
+    fi
 fi
+
