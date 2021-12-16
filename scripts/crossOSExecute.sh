0d404cd3da2c169fa4c9d6c96c9a4c5a90835d21
MenkeTechnologies <linux.dev25@gmail.com>
2021-12-15 23:01:10 -0500

    add cross OS execute

---
+++ scripts/crossOSExecute.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+#{{{                    MARK:Header
+#**************************************************************
+##### Author: JACOBMENKE
+##### Date: Wed Dec 15 22:51:36 EST 2021
+##### Purpose: bash script to execute code base on os
+##### Notes:
+#}}}***********************************************************
+
+
+function zpwrOsRaspbianVsFamily() {
+
+    case $distroName in
+            (raspbian)
+                eval "$1"
+                ;;
+            (debian | ubuntu* | elementary* | kali | linuxmint | zorin | parrot)
+                eval "$2"
+                ;;
+            (fedora | ol | centos | rhel | amzn)
+                eval "$3"
+                ;;
+            (*suse* | arch | endeavouros | garuda | manjaro*)
+                eval "$4"
+                ;;
+            (*)
+                eval "$5"
+                ;;
+        esac
+}
+
+
+function zpwrOsDebVsUbuntu() {
+
+    case $distroName in
+        (debian | raspbian | kali | parrot | zorin)
+            eval "$1"
+            ;;
+        (ubuntu* | linuxmint | pop* | elementary*)
+            eval "$2"
+            ;;
+        (fedora | ol | centos | rhel | amzn)
+            eval "$3"
+            ;;
+        (arch | endeavouros | garuda | manjaro*)
+            eval "$4"
+            ;;
+        (*suse*)
+            eval "$5"
+            ;;
+        (*)
+            eval "$6"
+            ;;
+    esac
+}
+
+function zpwrOsFamily() {
+
+    case $distroName in
+        (debian | ubuntu* | pop* | elementary* | raspbian | kali | linuxmint | zorin | parrot)
+            eval "$1"
+            ;;
+        (fedora | ol | centos | rhel | amzn)
+            eval "$2"
+            ;;
+        (arch | endeavouros | garuda | manjaro*)
+            eval "$3"
+            ;;
+        (*suse*)
+            eval "$4"
+            ;;
+        (*)
+            eval "$5"
+            ;;
+    esac
+}
