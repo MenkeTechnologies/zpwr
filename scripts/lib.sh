90a8fbe1e292cdef6a8f1fedd30c83bc579a53d0
MenkeTechnologies <linux.dev25@gmail.com>
2022-01-13 15:49:58 -0500

    zpwrExists supports multiple args

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -19,16 +19,32 @@ if zpwrIsZsh; then
     if ! type -a -- zpwrExists>/dev/null 2>&1; then
 
         function zpwrExists(){
+
+            if [[ -z "$1" ]]; then
+                zpwrLogConsoleErr "usage: zpwrExists <item...>"
+                return 1
+            fi
+            local i
             #alternative is command -v
-            type -a -- "$1" &>/dev/null || return 1 &&
-            [[ $(type -a -- "$1" 2>/dev/null) != *"suffix alias"* ]]
+            for i in "$@";do
+                type -a -- "$i" &>/dev/null || return 1 &&
+                [[ $(type -a -- "$i" 2>/dev/null) != *"suffix alias"* ]]
+            done
         }
     fi
 else
     function zpwrExists(){
 
+        if [[ -z "$1" ]]; then
+            zpwrLogConsoleErr "usage: zpwrExists <item...>"
+            return 1
+        fi
+        local i
+
         #alternative is command -v
-        type -a -- "$1" >/dev/null 2>&1
+        for i in "$@";do
+            type -a -- "$i" >/dev/null 2>&1
+        done
     }
 fi
 
@@ -45,6 +61,12 @@ function zpwrStdinExists(){
 }
 
 function zpwrCommandExists(){
+
+    if [[ -z "$1" ]]; then
+        zpwrLogConsoleErr "usage: zpwrCommandExists <command...>"
+        return 1
+    fi
+    local cmd
 
     for cmd in "$@"; do
         type -ap -- "$cmd" >/dev/null 2>&1 || return 1
