c2a1fd672704fa587ae73ed218282b6b6c3966b1
MenkeTechnologies <linux.dev25@gmail.com>
2022-05-17 23:21:14 -0400

    add df image verb with completion

--- scripts/zpwr.zsh
+++ scripts/zpwr.zsh
@@ -81,6 +81,7 @@ ZPWR_VARS[VERB_0]="$0"
     ZPWR_VERBS[detach]='zpwrDetachall=detach from all tmux sessions'
     ZPWR_VERBS[digs]='digs=run series on networking commands on arg'
     ZPWR_VERBS[dirsearch]='zpwrFzfDirsearchVerb=cd to a sub dir'
+    ZPWR_VERBS[dfimage]='zpwrDfimage=cat Dockerfile from IMAGE_ID'
     ZPWR_VERBS[exercism]='zpwrOpenExercism=go to Exercism home page'
     ZPWR_VERBS[homeexercism]='cd $HOME/Exercism=cd to $HOME/Exercism'
     ZPWR_VERBS[exercismdownload]='zpwrExercismDownloadAll=download all for track'
