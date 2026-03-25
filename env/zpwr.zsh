d47131b1f7731d1191cdb9c2816c8e84392457e0
MenkeTechnologies <linux.dev25@gmail.com>
2026-03-25 07:53:41 -0400

    zpwr help

--- env/zpwr.zsh
+++ env/zpwr.zsh
@@ -310,6 +310,7 @@ ZPWR_VARS[VERB_0]="$0"
     ZPWR_VERBS[subcommandsedit]='zpwrVerbsEditNoZLE=edit the subcommands for zpwr <tab>'
     ZPWR_VERBS[subcommandsfzf]='zpwrVerbsFZF=fzf the subcommands for zpwr <tab>'
     ZPWR_VERBS[subcommandslist]='zpwrListVerbs=the subcommands for zpwr <tab>'
+    ZPWR_VERBS[help]='zpwrListVerbs=the subcommands for zpwr <tab>'
     ZPWR_VERBS[subcommandscount]='zpwrNumVerbs=number of choice for zpwr <tab>'
     ZPWR_VERBS[taillog]='lo=tail -F $ZPWR_LOGFILE'
     ZPWR_VERBS[test]='zpwrTest=run all zpwr tests'
